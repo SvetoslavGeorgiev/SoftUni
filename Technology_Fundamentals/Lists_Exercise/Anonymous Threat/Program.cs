c66c9671cdd0f07f700fca49e8c27bdaaf348d7a
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2019-11-01 18:15:43 +0000

    Completing "Anonymous Threat" project.

--- Technology_Fundamentals/Lists_Exercise/Anonymous Threat/Program.cs
+++ Technology_Fundamentals/Lists_Exercise/Anonymous Threat/Program.cs
@@ -20,20 +20,24 @@ namespace Problem07AppendArrays
                 int start = Math.Max(startIndex, 0);
                 int endIndex = int.Parse(temp[2]);
                 int end = Math.Min(endIndex, list.Count);
+                if (end == list.Count)
+                {
+                    end = end - 1;
+                }
                 int index = int.Parse(temp[1]);
                 int partitions = int.Parse(temp[2]);
 
                 if (temp[0] == "merge")
                 {
                     List<string> merge = new List<string>();
-                    for (int i = start; i < end; i++)
+                    for (int i = start; i <= end; i++)
                     {
                         merge.Add(list[i]);
-                        
+
                     }
                     if (start < end)
                     {
-                        int count = end - start;
+                        int count = end - start + 1;
                         list.RemoveRange(start, count);
                         string add = string.Join("", merge);
 
@@ -42,22 +46,34 @@ namespace Problem07AppendArrays
                 }
                 else
                 {
-                    //List<char> devide = list[index].Split().Select(char.Parse).ToList();
+                    string stringForDevition = list[index];
 
+                    list.RemoveAt(index);
+                    var stringToAdd = new List<string>();
+                    int partitionLength = stringForDevition.Length / partitions;
+                    int remainder = stringForDevition.Length % partitions;
 
-                    //Console.WriteLine();
+                    string token = string.Empty;
+                    int count = 0;
 
-
+                    for (int i = 0; i < stringForDevition.Length; i++)
+                    {
+                        token += stringForDevition[i];
+                        if (token.Length == partitionLength)
+                        {
+                            stringToAdd.AddRange(token.Split().ToList());
+                            token = string.Empty;
+                            count++;
+                            if (count == partitions - 1)
+                            {
+                                partitionLength += remainder;
+                            }
+                        }
+                    }
+                    list.InsertRange(index, stringToAdd);
                 }
-
-
-
-
-
-
-
                 command = Console.ReadLine();
-            }
+            }   
             Console.WriteLine(string.Join(" ", list));
         }
     }
