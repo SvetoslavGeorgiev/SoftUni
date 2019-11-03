251413e1440a12cbde3d8a03d8056f8cf99c6564
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2019-11-03 21:34:46 +0000

    Added new Solution and few new projects.

---
+++ Technology_Fundamentals/Objects_and_Classes_Exercise/Students/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
