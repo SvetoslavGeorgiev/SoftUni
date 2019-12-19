3da9569b77bc0e368c3f485ae3be967a93113380
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2019-12-19 20:57:53 +0000

    Added few new projects.

---
+++ Technology_Fundamentals/Associative_Arrays_Lab/Count_Real_Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
