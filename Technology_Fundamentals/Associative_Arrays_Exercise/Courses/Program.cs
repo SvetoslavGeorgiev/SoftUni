68cf7e525bc64b724683f7054e483e8f7a59db40
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2019-12-26 13:48:51 +0000

    finished a project!!

--- Technology_Fundamentals/Associative_Arrays_Exercise/Courses/Program.cs
+++ Technology_Fundamentals/Associative_Arrays_Exercise/Courses/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Courses
 {
@@ -6,7 +8,36 @@ namespace Courses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var command = string.Empty;
+
+            var peopleList = new List<string>();
+
+            var coursesDictionary = new Dictionary<string, List<string>>();
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                var splittedCommand = command.Split(" : ");
+
+                if (coursesDictionary.ContainsKey(splittedCommand[0]))
+                {
+                    coursesDictionary[splittedCommand[0]].Add(splittedCommand[1]);
+                }
+                else
+                {
+                    coursesDictionary.Add(splittedCommand[0], peopleList);
+                    coursesDictionary[splittedCommand[0]].Add(splittedCommand[1]);
+                }
+                peopleList = new List<string>();
+            }
+
+            foreach (var course in coursesDictionary.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var people in course.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {people}");
+                }
+            }
         }
     }
 }
