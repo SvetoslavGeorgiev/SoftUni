20026b0e8817d4b5435a5eb2dc2b109acbe84191
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-01-07 20:41:34 +0000

    Added new project Extract_File.

---
+++ Extract_File/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
