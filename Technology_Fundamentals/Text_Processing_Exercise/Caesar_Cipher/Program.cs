1fcc73d7f37bdb9bba393a51f61dff9a30bb7a5c
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-01-13 09:14:46 +0000

    Added project Caesar_Cipher.

---
+++ Technology_Fundamentals/Text_Processing_Exercise/Caesar_Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var encryptedText = new StringBuilder();
+
+
+            foreach (var item in input)
+            {
+                var newChar = (item + 3);
+                encryptedText.Append((char)newChar);
+            }
+
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
