292fc8e60010fce1dc5123d039c5fe2db196149d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-01-13 23:25:01 +0000

    Added two more projects and finish old one!!

---
+++ Technology_Fundamentals/Text_Processing_Exercise/Furniture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Technology_Fundamentals/Text_Processing_Exercise/Letters_Change_Numbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            var list = text.Split(new char[] { '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var sb = new StringBuilder();
+
+            var numberInAlphabet = 0M;
+
+            var sum = 0M;
+            foreach (var word in list)
+            {
+                decimal subSum = 0;
+                foreach (var item in word)
+                {
+                    if (char.IsDigit(item))
+                    {
+                        sb.Append(item);
+                    }
+                }
+                var number = decimal.Parse(sb.ToString());
+                if (char.IsUpper(word[0]))
+                {
+                    numberInAlphabet = word[0] - 64;
+                    subSum += number / numberInAlphabet;
+
+                }
+                else
+                {
+                    numberInAlphabet = word[0] - 96;
+                    subSum += number * numberInAlphabet;
+                }
+                if (char.IsUpper(word[word.Length - 1]))
+                {
+                    numberInAlphabet = word[word.Length - 1] - 64;
+                    subSum -= numberInAlphabet;
+                }
+                else
+                {
+                    numberInAlphabet = word[word.Length - 1] - 96;
+                    subSum += numberInAlphabet;
+                }
+                sum += subSum;
+                subSum = 0;
+                sb.Clear();
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
