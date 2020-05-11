f7e612206241473d0ba09d8b727a3fb4972fd77e
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-05-11 20:45:42 +0100

    Added Two new projects!

---
+++ Technology_Fundamentals/Lab_Basic_Web/MyFirstASPNETCORE/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETCORE.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult List()
+        {
+            ViewData["Name"] = "Светослав Георгиев";
+            return View();
+        }
+    }
+}
---
+++ Technology_Fundamentals/Lab_Basic_Web/MyFirstASPNETCORE/Models/StudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETCORE.Models
+{
+    public class StudentViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
