5c0a77ee6eb09e6a150e0bf9ac1d34606d0ed183
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-06-02 17:28:53 +0100

    Added few projects and the problems in that solution!

---
+++ CSharp_Avanced/Multidimensional_Arrays_Exercise/Matrix_Shuffling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+
+            for (int row = 0; row < matrixSize[0]; row++)
+            {
+                var command = new Queue<int>(Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray());
+
+                for (int col = 0; col < matrixSize[1]; col++)
+                {
+
+                    matrix[row, col] = command.Dequeue();
+                }
+            }
+
+            var command2 = string.Empty;
+            while ((command2 = Console.ReadLine().ToLower()) != "end")
+            {
+                var currentCommand = command2.Split().ToArray();
+
+                if (currentCommand[0] != "swap")
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+
+                }
+
+
+
+
+            }
+        }
+    }
+}
