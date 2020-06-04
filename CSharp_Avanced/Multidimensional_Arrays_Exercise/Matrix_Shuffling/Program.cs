1181937ba63dc9b41555158a49bf1f0484879205
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-06-04 11:40:31 +0100

    Finish one and add one more projects!

--- CSharp_Avanced/Multidimensional_Arrays_Exercise/Matrix_Shuffling/Program.cs
+++ CSharp_Avanced/Multidimensional_Arrays_Exercise/Matrix_Shuffling/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace Matrix_Shuffling
@@ -9,15 +10,14 @@ namespace Matrix_Shuffling
     {
         static void Main(string[] args)
         {
-            var matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var matrixSize = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            string[,] matrix = new string[matrixSize[0], matrixSize[1]];
 
             for (int row = 0; row < matrixSize[0]; row++)
             {
-                var command = new Queue<int>(Console.ReadLine()
+                var command = new Queue<string>(Console.ReadLine()
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
                     .ToArray());
 
                 for (int col = 0; col < matrixSize[1]; col++)
@@ -38,13 +38,55 @@ namespace Matrix_Shuffling
                 }
                 else
                 {
+                    if (!IsNumber(currentCommand[1]) ||
+                        !IsNumber(currentCommand[2]) ||
+                        !IsNumber(currentCommand[3]) ||
+                        !IsNumber(currentCommand[4]))
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    else
+                    {
+                        var row1 = int.Parse(currentCommand[1]);
+                        var col1 = int.Parse(currentCommand[2]);
+                        var row2 = int.Parse(currentCommand[3]);
+                        var col2 = int.Parse(currentCommand[4]);
 
+                        if (row1 < 0 || row1 >= matrix.GetLength(0) || row2 < 0 || row2 >= matrix.GetLength(0) ||
+                            col1 < 0 || col1 >= matrix.GetLength(1) || col2 < 0 || col2 >= matrix.GetLength(1))
+                        {
+                            Console.WriteLine("Invalid input!");
+                        }
+                        else
+                        {
+                            var temp = matrix[row1, col1];
+
+                            matrix[row1, col1] = matrix[row2, col2];
+
+                            matrix[row2, col2] = temp;
+
+
+                            for (int row = 0; row < matrixSize[0]; row++)
+                            {
+                                for (int col = 0; col < matrixSize[1]; col++)
+                                {
+                                    Console.Write($"{matrix[row, col]} ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                    
                 }
-
-
-
-
             }
+        }
+
+        private static bool IsNumber(string text)
+        {
+            int test;
+
+            return int.TryParse(text, out test);
         }
     }
 }
---
+++ CSharp_Avanced/Multidimensional_Arrays_Exercise/Snake_Moves/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
