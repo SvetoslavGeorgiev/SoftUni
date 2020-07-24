c482bf8c29f26bdbf97492e6e05506a5b6f638dc
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-07-24 23:07:19 +0100

    Two new projects!

---
+++ CSharp_Avanced/Sets_and_Dictionaries_Advanced_Exercise/Sets_of_Elements/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] set = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+            var nSet = new HashSet<int>();
+
+            for (int i = 0; i < set[0]; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                firstSet.Add(currentNumber);
+            }
+            for (int i = 0; i < set[1]; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                secondSet.Add(currentNumber);
+            }
+            
+
+
+            if (firstSet.Count >= secondSet.Count)
+            {
+                foreach (var number in secondSet)
+                {
+                    if (firstSet.Contains(number))
+                    {
+                        nSet.Add(number);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var number in firstSet)
+                {
+                    if (secondSet.Contains(number))
+                    {
+                        nSet.Add(number);
+                    }
+                }
+            }
+
+
+            foreach (var number in nSet)
+            {
+                Console.Write($"{number} ");
+            }
+
+        }
+    }
+}
---
+++ CSharp_Avanced/Sets_and_Dictionaries_Advanced_Exercise/Unique_Usernames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            var names = new HashSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+
+                var currentName = Console.ReadLine();
+
+                names.Add(currentName);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
