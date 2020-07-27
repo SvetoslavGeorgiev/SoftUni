cb1b20d93246f7fb7d0b8a9bd9d65a9d1c77179a
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-07-27 09:00:36 +0100

    Added a few new projects.

---
+++ CSharp_Avanced/Streams_Files_and_Directories_Lab/Word_Count/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWords = File.ReadAllText("03. Word Count/words.txt");
+            var input = inputWords.Split().ToArray();
+
+            string pattern = @"[a-z]+";
+            Regex regex = new Regex(pattern);
+
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                dict.Add(input[i], 0);
+            }
+
+            using (var reader = new StreamReader("03. Word Count/text.txt"))
+            {
+                var currentLine = string.Empty;
+
+                while (((currentLine = reader.ReadLine()) != null))
+                {
+
+                    var matches = regex.Matches(currentLine.ToLower());
+
+                    foreach (Match word in matches)
+                    {
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            if (word.Value == input[i])
+                            {
+                                dict[input[i]]++;
+                            }
+                        }
+                    }
+                    //var sentence = currentLine.ToLower().Split(new char[] { ',', ' ', '-', '.'  }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                    //foreach (var element in sentence)
+                    //{
+                    //    for (int i = 0; i < input.Length; i++)
+                    //    {
+                    //        if (element == input[i])
+                    //        {
+                    //            dict[input[i]]++;
+                    //        }
+                    //    }
+                    //}
+                }
+            }
+            using (var writer = new StreamWriter("Output.txt"))
+            {
+                foreach (var kvp in dict.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+        }
+    }
+}
