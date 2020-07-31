3964d079c70c05e3eb47dc06a7d65702e3bfcb27
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-07-31 11:38:17 +0100

    Added two new files!!

---
+++ CSharp_Avanced/Streams_Files_and_Directories_Lab/Folder_Size/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = new DirectoryInfo("06. Folder Size/TestFolder");
+
+            double sum = 0;
+
+            foreach (var file in dir.GetFiles())
+            {
+                sum += file.Length;
+            }
+
+            var sumInMb = sum / 1024 / 1024;
+
+            File.WriteAllText("оutput.txt", sumInMb.ToString());
+
+        }
+    }
+}
