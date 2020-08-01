e8382d5a648a29cf97802275d7ae26008421c3e4
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-08-01 12:33:52 +0100

    Added few new projects.

---
+++ CSharp_Avanced/Streams_Files_and_Directories_Exercise/Even_Lines/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var counter = -1;
+
+            var chars = new char[] { '-', ',', '.', '!', '?' };
+
+            var stringBuilder = new StringBuilder();
+
+            using (var reader = new StreamReader("text.txt"))
+            {
+
+                var currentLine = string.Empty;
+                
+                using (var writer = new StreamWriter("output.txt"))
+                {
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        counter++;
+                        if (counter % 2 == 0)
+                        {
+                            foreach (var @char in currentLine)
+                            {
+                                if (chars.Contains(@char))
+                                {
+                                    stringBuilder.Append('@');
+                                }
+                                else
+                                {
+                                    stringBuilder.Append(@char);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        var newString = string.Join("", stringBuilder);
+                        stringBuilder.Clear();
+
+                        var stringArr = newString.Split().ToArray();
+
+                        writer.WriteLine(string.Join(" ", stringArr.Reverse()));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp_Avanced/Streams_Files_and_Directories_Exercise/Line_Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var counter = 0;
+
+            var chars = new char[] { '-', ',', '.', '!', '?' };
+
+
+            using (var reader = new StreamReader("text.txt"))
+            {
+
+                var currentLine = string.Empty;
+
+                using (var writer = new StreamWriter("output.txt"))
+                {
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        counter++;
+                        var whiteSpace = 0;
+                        var symbolCount = 0;
+
+                        foreach (var @char in currentLine)
+                        {
+                            if (char.IsWhiteSpace(@char))
+                            {
+                                whiteSpace++;
+                            }
+                            else if (!char.IsLetterOrDigit(@char))
+                            {
+                                symbolCount++;
+                            }
+                        }
+                        var allChars = currentLine.Length - whiteSpace - symbolCount;
+
+                        writer.WriteLine($"Line {counter}: {currentLine} ({allChars})({symbolCount})");
+                    }
+                }
+            }
+        }
+    }
+}
