b267c39e9f1febd8a3e7f3bdb20b2e2d2669a347
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-08-15 13:31:52 +0100

    solved few new problems!

---
+++ CSharp_Avanced/Functional_Programming_Exercise/Functional_Programming_Exercise/Custom_Min_Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var input = Console
+            //    .ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .Min(x => x);
+
+            //Console.WriteLine(input);
+
+
+            var input = Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            Func<List<int>, int> checker = x => x.Min();
+
+            var number = checker(input);
+
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ CSharp_Avanced/Functional_Programming_Exercise/Functional_Programming_Exercise/Knights_of_Honor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+
+            Action<string> print = Print(input);
+
+            input.ForEach(print);
+        }
+
+        private static Action<string> Print(List<string> input)
+        {
+            return name => Console.WriteLine($"Sir {name}");
+        }
+    }
+}
