93944f50279a35a62a842438ed7fe2eccdd0c98f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2020-09-04 16:32:00 +0100

    finish one and added another project.

---
+++ CSharp_Avanced/Functional_Programming_Exercise/Predicate_For_Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requiredLength = int.Parse(Console.ReadLine());
+
+            Func<string, bool> maxLenght = MaxLennght(requiredLength);
+
+            Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(maxLenght)
+                .ToList()
+                .ForEach(Console.WriteLine);
+            
+        }
+
+        private static Func<string, bool> MaxLennght(int requiredLength)
+        {
+            return x => x.Length <= requiredLength;
+        }
+    }
+}
---
+++ CSharp_Avanced/Functional_Programming_Exercise/Reverse_and_Exclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var numberToDivideWith = int.Parse(Console.ReadLine());
+
+
+            var reversedListv = numbers.Where(x => x % numberToDivideWith != 0).Reverse();
+
+
+            Console.WriteLine(string.Join(" ", reversedListv));
+
+
+
+        }
+    }
+}
