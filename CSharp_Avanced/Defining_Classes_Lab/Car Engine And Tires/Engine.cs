23b81bef1c7db0ad4ea9711156d35bf5f3bb9280
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2021-04-17 07:13:16 +0100

    Added new project.

---
+++ CSharp_Avanced/Defining_Classes_Lab/Car Engine And Tires/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+    }
+}
