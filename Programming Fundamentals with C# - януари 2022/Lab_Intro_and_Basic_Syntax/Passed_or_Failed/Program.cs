5a9231c73f5778abce45afd65f4a1a3d2042edfd
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-01-15 01:40:57 +0000

    few new projects.

---
+++ Programming Fundamentals with C# - януари 2022/Lab_Intro_and_Basic_Syntax/Passed_or_Failed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Passed_or_Failed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var StudentGradeForChekingIfPass = double.Parse(Console.ReadLine());
+
+            if (StudentGradeForChekingIfPass >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
--- Programming Fundamentals with C# - януари 2022/Lab_Intro_and_Basic_Syntax/Student_Information/Program.cs
+++ Programming Fundamentals with C# - януари 2022/Lab_Intro_and_Basic_Syntax/Student_Information/Program.cs
@@ -6,7 +6,11 @@ namespace Student_Information
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            var studentName = Console.ReadLine();
+            var studentAge = int.Parse(Console.ReadLine());
+            var studentAverageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {studentAverageGrade:F2}"); 
         }
     }
 }
