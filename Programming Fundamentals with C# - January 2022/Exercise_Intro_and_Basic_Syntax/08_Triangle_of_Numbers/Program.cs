33c367c2d709276ab9fc07ce4c2ff3c3f6ab151a
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-01-21 11:47:31 +0000

    Adding few new project in Intro_and_Basic_Syntax

---
+++ Programming Fundamentals with C# - January 2022/Exercise_Intro_and_Basic_Syntax/08_Triangle_of_Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08_Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
