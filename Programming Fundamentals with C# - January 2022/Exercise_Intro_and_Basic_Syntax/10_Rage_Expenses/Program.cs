a837bc9ef12e20622986b2a8e69b8bb4e424d71e
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-01-23 11:45:17 +0000

    Added last few project in Exerxise_Intro_and_Basic_Syntax

---
+++ Programming Fundamentals with C# - January 2022/Exercise_Intro_and_Basic_Syntax/10_Rage_Expenses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _10_Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lostGames = int.Parse(Console.ReadLine());
+            var headsetPrice = decimal.Parse(Console.ReadLine());
+            var mousePrice = decimal.Parse(Console.ReadLine());
+            var keyboardPrice = decimal.Parse(Console.ReadLine());
+            var displayPrice = decimal.Parse(Console.ReadLine());
+
+            var trashedHeadsetsCount = 0;
+            var trashedMousesCount = 0;
+            var trashedKeyboardCount = 0;
+            var trashedDisplayCount = 0;
+            var counterForDisplay = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    trashedHeadsetsCount++;
+                }
+                if (i % 3 == 0)
+                {
+                    trashedMousesCount++;
+                }
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    trashedKeyboardCount++;
+                    counterForDisplay++;
+                }
+                if (counterForDisplay == 2)
+                {
+                    trashedDisplayCount++;
+                    counterForDisplay = 0;
+                }
+            }
+
+            var totalExpenses = (trashedDisplayCount * displayPrice) + (trashedHeadsetsCount * headsetPrice) + (trashedKeyboardCount * keyboardPrice) + (trashedMousesCount * mousePrice);
+
+            Console.WriteLine($"Rage expenses: {totalExpenses:F2} lv.");
+
+
+        }
+    }
+}
---
+++ Programming Fundamentals with C# - January 2022/Exercise_Intro_and_Basic_Syntax/11_Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11_Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfOrders = int.Parse(Console.ReadLine());
+           
+
+            decimal priceForTheCoffee = 0;
+            decimal totalPriceForAllOrders = 0;
+
+            for (int i = 0; i < numberOfOrders; i++)
+            {
+                var pricePerCapsule = decimal.Parse(Console.ReadLine());
+                var daysInMonth = int.Parse(Console.ReadLine());
+                var capsuleCount = int.Parse(Console.ReadLine());
+                priceForTheCoffee = (daysInMonth * capsuleCount) * pricePerCapsule;
+                Console.WriteLine($"The price for the coffee is: ${priceForTheCoffee:F2}");
+                totalPriceForAllOrders += priceForTheCoffee;
+                priceForTheCoffee = 0;
+            }
+            Console.WriteLine($"Total: ${totalPriceForAllOrders:F2}");
+        }
+    }
+}
