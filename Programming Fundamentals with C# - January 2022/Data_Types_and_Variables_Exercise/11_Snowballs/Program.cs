eb8ac645b441c91c900f3a9f4a5821013150ccf0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-01-29 12:35:23 +0000

    Adding the last project in data_types_and_variables_exercise

---
+++ Programming Fundamentals with C# - January 2022/Data_Types_and_Variables_Exercise/11_Snowballs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11_Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfSnowballs = int.Parse(Console.ReadLine());
+
+            BigInteger snowBallValue = 0;
+            var forPrintingSnowballSnow = 0;
+            var forPrintingSnowballTime = 0;
+            var forPrintingSnowballQuality = 0;
+
+            for (int i = 0; i < numberOfSnowballs; i++)
+            {
+                var snowballSnow = int.Parse(Console.ReadLine());
+                var snowballTime = int.Parse(Console.ReadLine());
+                var snowballQuality = int.Parse(Console.ReadLine());
+
+                
+                if (snowballTime != 0)
+                {
+                    BigInteger currentsSnowBallValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+                    if (currentsSnowBallValue >= snowBallValue)
+                    {
+                        snowBallValue = currentsSnowBallValue;
+                        forPrintingSnowballSnow = snowballSnow;
+                        forPrintingSnowballTime = snowballTime;
+                        forPrintingSnowballQuality = snowballQuality;
+
+                    }
+                }
+                
+            }
+            Console.WriteLine($"{forPrintingSnowballSnow} : {forPrintingSnowballTime} = {snowBallValue} ({forPrintingSnowballQuality})");
+        }
+    }
+}
