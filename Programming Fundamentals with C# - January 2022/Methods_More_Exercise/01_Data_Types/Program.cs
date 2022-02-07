4d0d7de1c8f13ac301c6438571b814e00d6b065b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-02-07 23:45:47 +0000

    adding a new folder - methods_more_exercise with few new projects

---
+++ Programming Fundamentals with C# - January 2022/Methods_More_Exercise/01_Data_Types/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            
+            if (input == "int")
+            {
+                var result = 0;
+                var number = int.Parse(Console.ReadLine());
+                result = number * 2;
+                Console.WriteLine(result);
+            }
+            else if(input == "real")
+            {
+                var result = 0.00;
+                var number = double.Parse(Console.ReadLine());
+                result = number * 1.5;
+                Console.WriteLine($"{result:F2}");
+            }
+            else
+            {
+                var str = Console.ReadLine();
+                Console.WriteLine($"${str}$");
+            }
+        }
+    }
+}
