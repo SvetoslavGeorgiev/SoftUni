3cb786549a64341f5f4c983be88cc3ea4abf41fe
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-02-19 13:22:01 +0000

    Adding the lats few projects in Arrys_More_Exercise

---
+++ Programming Fundamentals with C# - January 2022/Arrays_More_Exercise/03_Recursive_Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03_Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            
+            if (n == 1 || n == 2)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            var fibonacciArr = new long[n];
+            fibonacciArr[0] = 1;
+            fibonacciArr[1] = 1;
+            
+            for (int i = 2; i < n; i++)
+            {
+                fibonacciArr[i] = fibonacciArr[i - 1] + fibonacciArr[i - 2];
+            }
+
+            Console.WriteLine(fibonacciArr[n - 1]);
+        }
+    }
+}
---
+++ Programming Fundamentals with C# - January 2022/Arrays_More_Exercise/05_Longest_Increasing_Subsequence/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Longest_Increasing_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var solutionsOFEachPreviosNumbers = new int[arr.Length];
+            var maxSolutions = 0;
+            var previusBestSolutionIndex = new int[arr.Length];
+            var maxSolutionIndex = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var solution = 1;
+                var previusBestIndex = -1;
+                
+                for (int j = 0; j < i; j++)
+                {
+                    var prevSolutions = solutionsOFEachPreviosNumbers[j];
+                    if (arr[i] > arr[j] && solution <= prevSolutions)
+                    {
+                        solution = prevSolutions + 1;
+                        previusBestIndex = j;
+                    }
+                }
+                previusBestSolutionIndex[i] = previusBestIndex;
+                solutionsOFEachPreviosNumbers[i] = solution;
+                if (maxSolutions < solution)
+                {
+                    maxSolutions = solution;
+                    maxSolutionIndex = i;
+                }
+            }
+
+            var index = maxSolutionIndex;
+
+            var result = new List<int>();
+
+            while (index != -1)
+            {
+                var num = arr[index];
+                result.Add(num);
+                index = previusBestSolutionIndex[index];
+            }
+
+            result.Reverse();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
