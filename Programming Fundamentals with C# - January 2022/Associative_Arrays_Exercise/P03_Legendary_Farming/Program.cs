a9e31b6641fea3b6406bc1d6c4a5c2b42ec2f554
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-03-10 11:12:55 +0000

    Adding few new projects and complete the directory Associative_Arrays_Exercise

--- Programming Fundamentals with C# - January 2022/Associative_Arrays_Exercise/P03_Legendary_Farming/Program.cs
+++ Programming Fundamentals with C# - January 2022/Associative_Arrays_Exercise/P03_Legendary_Farming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P03_Legendary_Farming
 {
@@ -6,9 +8,75 @@ namespace P03_Legendary_Farming
     {
         static void Main(string[] args)
         {
-            
+            var keyMaterials = new Dictionary<string, int>();
+            var junkMaterials = new Dictionary<string, int>();
 
+            keyMaterials.Add("shards", 0);
+            keyMaterials.Add("motes", 0);
+            keyMaterials.Add("fragments", 0);
+            var firstToRach250 = string.Empty;
+            var obtained = string.Empty;
 
+            while (obtained == string.Empty)
+            {
+                var materials = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+                for (int i = 0; i < materials.Count; i += 2)
+                {
+                    var currentMatirial = materials[i + 1].ToLower();
+                    var currentQuantity = int.Parse(materials[i]);
+
+                    if (keyMaterials.ContainsKey(currentMatirial))
+                    {
+                        keyMaterials[currentMatirial] += currentQuantity;
+                        if (keyMaterials[currentMatirial] >= 250)
+                        {
+                            keyMaterials[currentMatirial] -= 250;
+                            firstToRach250 = currentMatirial;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (junkMaterials.ContainsKey(currentMatirial))
+                        {
+                            junkMaterials[currentMatirial] += currentQuantity;
+                        }
+                        else
+                        {
+                            junkMaterials.Add(currentMatirial, currentQuantity);
+                        }
+                    }
+                }
+
+                switch (firstToRach250)
+                {
+                    case "shards":
+                        obtained = "Shadowmourne";
+                        break;
+                    case "fragments":
+                        obtained = "Valanyr";
+                        break;
+                    case "motes":
+                        obtained = "Dragonwrath";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{obtained} obtained!");
+
+            foreach (var keyMaterial in keyMaterials)
+            {
+                Console.WriteLine($"{keyMaterial.Key}: {keyMaterial.Value}");
+            }
+
+            foreach (var junkMaterial in junkMaterials)
+            {
+                Console.WriteLine($"{junkMaterial.Key}: {junkMaterial.Value}");
+            }
         }
     }
 }
---
+++ Programming Fundamentals with C# - January 2022/Associative_Arrays_Exercise/P06_Courses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = string.Empty;
+
+            var peopleList = new List<string>();
+
+            var coursesDictionary = new Dictionary<string, List<string>>();
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                var splittedCommand = command.Split(" : ");
+
+                if (coursesDictionary.ContainsKey(splittedCommand[0]))
+                {
+                    coursesDictionary[splittedCommand[0]].Add(splittedCommand[1]);
+                }
+                else
+                {
+                    coursesDictionary.Add(splittedCommand[0], peopleList);
+                    coursesDictionary[splittedCommand[0]].Add(splittedCommand[1]);
+                }
+                peopleList = new List<string>();
+            }
+
+            foreach (var course in coursesDictionary)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var people in course.Value)
+                {
+                    Console.WriteLine($"-- {people}");
+                }
+            }
+        }
+    }
+}
