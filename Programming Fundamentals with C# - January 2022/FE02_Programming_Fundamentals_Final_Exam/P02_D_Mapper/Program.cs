85b3c04cd0a1e020c4b73759f7d8732e5e970a70
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-03-26 18:59:19 +0000

    Adding new project P02_D_Mapper in FE02_Programming_Fundamentals_Final_Exam

---
+++ Programming Fundamentals with C# - January 2022/FE02_Programming_Fundamentals_Final_Exam/P02_D_Mapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P02_D_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine();
+
+            var listOfDestinations = new List<string>();
+
+            var patern = @"(=|\/)([A-Z][A-Za-z]{2,})\1";
+
+            var matches = Regex.Matches(input, patern);
+
+            var sumOfThePoints = 0;
+
+            foreach (Match match in matches)
+            {
+                var currentDestination = match.Value.Trim(new[] { '=', '/' });
+
+                listOfDestinations.Add(currentDestination);
+                sumOfThePoints += currentDestination.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", listOfDestinations)}");
+            Console.WriteLine($"Travel Points: {sumOfThePoints}");
+        }
+    }
+}
