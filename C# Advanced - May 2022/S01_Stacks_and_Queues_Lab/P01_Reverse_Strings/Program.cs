9b9d099df266afc33441bbbff159b1cbaec29bdc
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-04-19 12:41:33 +0100

    Creating new course folder - /C# Advanced - May 2022/ and adding a new project - P01_Reverse_Strings in it

---
+++ C# Advanced - May 2022/S01_Stacks_and_Queues_Lab/P01_Reverse_Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in command)
+            {
+                stack.Push(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
