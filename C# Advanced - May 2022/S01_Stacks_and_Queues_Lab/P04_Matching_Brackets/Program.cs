38072457a6153200de76be64024f33caf0444a25
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-04-26 09:17:31 +0100

    Adding new project P04_Matching_Brackets

---
+++ C# Advanced - May 2022/S01_Stacks_and_Queues_Lab/P04_Matching_Brackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> symbols = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    symbols.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    var openBracetIndex = symbols.Pop();
+
+                    Console.WriteLine(input.Substring(openBracetIndex, i - openBracetIndex + 1));
+                }
+            }
+        }
+    }
+}
