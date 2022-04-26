86220639ddbae4a335e9d48deeb4225715ac3bbb
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-04-26 09:11:27 +0100

    Adding new project P03_Simple_Calculator

---
+++ C# Advanced - May 2022/S01_Stacks_and_Queues_Lab/P03_Simple_Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+
+            var stack = new Stack<string>(input.Reverse());
+
+            var sum = int.Parse(stack.Pop());
+
+            while (stack.Any())
+            {
+
+                if (stack.Pop() == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
