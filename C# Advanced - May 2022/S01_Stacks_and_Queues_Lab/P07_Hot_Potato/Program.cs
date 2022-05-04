1d5c2c1485068b2dbca70f79595918643e24f115
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-04 12:55:09 +0100

    Added P07_Hot_Potato

---
+++ C# Advanced - May 2022/S01_Stacks_and_Queues_Lab/P07_Hot_Potato/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allChildren = Console.ReadLine().Split();
+
+            var children = new Queue<string>(allChildren);
+
+            var number = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (children.Count > 1)
+            {
+                while (number != counter)
+                {
+                    if (counter == number - 1)
+                    {
+                        Console.WriteLine($"Removed {children.Dequeue()}");
+                    }
+                    else
+                    {
+                        var currentChild = children.Dequeue();
+                        children.Enqueue(currentChild);
+                    }
+                    counter++;
+                }
+                counter = 0;
+            }
+
+            Console.WriteLine($"Last is {children.Peek()}");
+        }
+    }
+}
