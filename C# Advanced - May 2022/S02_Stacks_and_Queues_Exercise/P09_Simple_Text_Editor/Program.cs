d0ffc4657ebcef5c714354d90412cbfdeec3d36d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 20:05:08 +0100

    project P09_Simple_Text_Editor

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P09_Simple_Text_Editor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P09_Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfOperations = int.Parse(Console.ReadLine());
+
+            Stack<string> updates = new Stack<string>();
+
+            StringBuilder text = new StringBuilder();
+
+            updates.Push(text.ToString());
+
+            for (int i = 1; i <= numberOfOperations; i++)
+            {
+
+                var command = Console.ReadLine().Split().ToArray();
+
+                if (command[0] == "1")
+                {
+
+                    text.Append(command[1]);
+                    updates.Push(text.ToString());
+
+                }
+                else if (command[0] == "2")
+                {
+                    var countToErase = int.Parse(command[1]);
+
+                    if (countToErase >= text.Length)
+                    {
+                        text.Clear();
+                        updates.Push(text.ToString());
+                        //Console.WriteLine(updates.Peek());
+                    }
+                    else
+                    {
+                        text.Remove(text.Length - countToErase, countToErase);
+                        updates.Push(text.ToString());
+                    }
+                }
+                else if (command[0] == "3")
+                {
+                    var charToPeek = int.Parse(command[1]);
+                    Console.WriteLine(text[charToPeek - 1]);
+                }
+                else
+                {
+                    text.Clear();
+                    updates.Pop();
+                    text.Append(updates.Peek());
+                }
+            }
+        }
+    }
+}
