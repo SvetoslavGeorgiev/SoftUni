eb137f0c135c8653f7f43b47d0a6ff62ca848c1f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 20:09:39 +0100

    solution P10_Crossroads

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P10_Crossroads/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P10_Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var greenLight = int.Parse(Console.ReadLine());
+
+            var freeWindow = int.Parse(Console.ReadLine());
+
+            var commandOrCar = string.Empty;
+
+            var counter = 0;
+
+            Queue<string> carsAtTheLigth = new Queue<string>();
+
+            while ((commandOrCar = Console.ReadLine()) != "END")
+            {
+
+                if (commandOrCar != "green")
+                {
+                    carsAtTheLigth.Enqueue(commandOrCar);
+                }
+                else
+                {
+                    var currentCar = string.Empty;
+                    var currentGreenLight = greenLight;
+
+                    while (currentGreenLight > 0 && carsAtTheLigth.Count > 0)
+                    {
+                        currentCar = carsAtTheLigth.Dequeue();
+                        counter++;
+                        currentGreenLight -= currentCar.Length;
+                    }
+                    if (currentGreenLight == 0)
+                    {
+                        continue;
+                    }
+                    if (currentGreenLight < 0)
+                    {
+
+                        currentGreenLight += freeWindow;
+
+                        if (currentGreenLight >= 0)
+                        {
+                            //counter++;
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currentCar} was hit at {currentCar[currentCar.Length + currentGreenLight]}.");
+                            return;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{counter} total cars passed the crossroads.");
+        }
+    }
+}
