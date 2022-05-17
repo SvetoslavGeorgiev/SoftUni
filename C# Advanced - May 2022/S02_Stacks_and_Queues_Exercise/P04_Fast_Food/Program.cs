2d0862b3438f157459a16e4b021005cb7638b0c4
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 16:26:11 +0100

    P04_Fast_Food

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P04_Fast_Food/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodQuantity = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> orders = new Queue<int>(input);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+
+                var currentOrder = orders.Peek();
+
+                if (currentOrder <= foodQuantity)
+                {
+                    orders.Dequeue();
+                    foodQuantity -= currentOrder;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
