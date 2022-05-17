1b2cdb9cfbce4c5f96e7d5499a0ce90cfaf329c2
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 14:40:12 +0100

    P03_Maximum_and_Minimum_Element

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P03_Maximum_and_Minimum_Element/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03_Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfActions = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>();
+
+
+            for (int i = 0; i < numberOfActions; i++)
+            {
+                var action = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (action[0] == 1)
+                {
+                    stack.Push(action[1]);
+                }
+                else if (action[0] == 2)
+                {
+                    if (stack.Any())
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (action[0] == 3)
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+
+        }
+    }
+}
