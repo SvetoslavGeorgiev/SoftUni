f6238148283eb342dc9301b2dfa32238deac80d6
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 19:34:39 +0100

    adding solution P06_Songs_Queue

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P06_Songs_Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P06_Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ");
+
+            Queue<string> playList = new Queue<string>(input);
+
+            while (playList.Any())
+            {
+                var command = Console.ReadLine().Split();
+
+                if (command[0] == "Play")
+                {
+                    playList.Dequeue();
+                }
+                else if (command[0] == "Show")
+                {
+                    if (playList.Any())
+                    {
+                        Console.WriteLine(string.Join(", ", playList));
+                    }
+                }
+                else
+                {
+                    var que = new Queue<string>(command);
+                    que.Dequeue();
+                    var song = string.Join(" ", que);
+                    if (!playList.Contains(song))
+                    {
+                        playList.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P07_Truck_Tour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P07_Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
