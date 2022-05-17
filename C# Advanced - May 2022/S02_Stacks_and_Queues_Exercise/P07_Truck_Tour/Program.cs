096f0b4e3734f923e6bd1179eb68745de3d6d0ba
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 19:41:17 +0100

    adding solution P07_Truck_Tour

--- C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P07_Truck_Tour/Program.cs
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P07_Truck_Tour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P07_Truck_Tour
 {
@@ -6,7 +8,62 @@ namespace P07_Truck_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numberOfPumpa = int.Parse(Console.ReadLine());
+
+            Queue<int> circle = new Queue<int>();
+
+            var difference = 0;
+
+            for (int i = 0; i < numberOfPumpa; i++)
+            {
+                var pump = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                difference = pump[0] - pump[1];
+
+                circle.Enqueue(difference);
+            }
+
+            var petrol = 0;
+            var index = int.MaxValue;
+            var currentPump = 0;
+
+            for (int i = 1; i <= numberOfPumpa; i++)
+            {
+                var newCircle = new Queue<int>(circle);
+                for (int k = 1; k < i; k++)
+                {
+                    newCircle.Enqueue(newCircle.Dequeue());
+                }
+
+                for (int j = 0; j <= newCircle.Count; j++)
+                {
+                    currentPump += newCircle.Dequeue();
+                    petrol = currentPump;
+                    if (petrol < 0)
+                    {
+                        index = int.MaxValue;
+                        break;
+                    }
+                    else if (petrol >= 0)
+                    {
+                        if (index >= i)
+                        {
+                            index = i - 1;
+                        }
+                    }
+                    if (newCircle.Count == 0)
+                    {
+                        Console.WriteLine(index);
+                        return;
+                    }
+                    j = -1;
+                }
+
+                currentPump = 0;
+            }
         }
     }
 }
