613676c7804497c737f381d152bb6ef1044e7429
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-17 17:08:27 +0100

    adding solution P05_Fashion_Boutique

---
+++ C# Advanced - May 2022/S02_Stacks_and_Queues_Exercise/P05_Fashion_Boutique/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList());
+
+            var rackCapacity = int.Parse(Console.ReadLine());
+
+
+            var rackCount = 0;
+            while (stack.Any())
+            {
+                var currentSum = 0;
+
+                var currentquantity = stack.Peek();
+
+                while (currentSum + currentquantity <= rackCapacity)
+                {
+                    
+                    if (currentSum + currentquantity <= rackCapacity)
+                    {
+                        currentSum += stack.Pop();
+                    }
+                    if (stack.Any())
+                    {
+                        currentquantity = stack.Peek();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                rackCount++;
+                
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
