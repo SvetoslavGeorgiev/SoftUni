b4b66bab0a312cce3ef1082aa1ec9e67f379aff0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-18 17:14:28 +0100

    solution P06_Jagged_Array_Modification

--- C# Advanced - May 2022/P01_Sum_Matrix_Elements/P06_Jagged_Array_Modification/Program.cs
+++ C# Advanced - May 2022/P01_Sum_Matrix_Elements/P06_Jagged_Array_Modification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P06_Jagged_Array_Modification
 {
@@ -6,7 +7,53 @@ namespace P06_Jagged_Array_Modification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = int.Parse(Console.ReadLine());
+            int[][] arr = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                arr[i] = currentRow;
+            }
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var commandArr = command.ToLower().Split().ToArray();
+
+                if (int.Parse(commandArr[1]) >= 0 &&
+                    int.Parse(commandArr[2]) >= 0 &&
+                    int.Parse(commandArr[1]) < rows &&
+                    int.Parse(commandArr[2]) < arr[int.Parse(commandArr[1])].Length)
+                {
+                    if (commandArr[0] == "add")
+                    {
+                        arr[int.Parse(commandArr[1])][int.Parse(commandArr[2])] += int.Parse(commandArr[3]);
+                    }
+                    else if (commandArr[0] == "subtract")
+                    {
+                        arr[int.Parse(commandArr[1])][int.Parse(commandArr[2])] -= int.Parse(commandArr[3]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < arr[row].Length; col++)
+                {
+                    Console.Write($"{arr[row][col]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
