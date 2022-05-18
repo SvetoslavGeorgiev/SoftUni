5d19ce5b73bd5a60649087d9644e5ff2fc1f9129
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-18 17:02:55 +0100

    solution P03_Primary_Diagonal

---
+++ C# Advanced - May 2022/P01_Sum_Matrix_Elements/P04_Symbol_in_Matrix/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P04_Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                var element = new Queue<char>(Console.ReadLine().ToCharArray());
+
+                for (int c = 0; c < size; c++)
+                {
+                    matrix[r, c] = element.Dequeue();
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            var counter = 0;
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        counter++;
+                        return;
+                    }
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix ");
+            }
+        }
+    }
+}
