3c4ad734b95345f45134dd5bb0c3776835993bfb
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-18 17:11:35 +0100

    solution P05_Square_With_Maximum_Sum

---
+++ C# Advanced - May 2022/P01_Sum_Matrix_Elements/P05_Square_With_Maximum_Sum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_Square_With_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int row = 0; row < size[0]; row++)
+            {
+                var element = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = element.Dequeue();
+                }
+            }
+
+
+
+            int maxRow = 0;
+            int maxCol = 0;
+
+            var maxSum = int.MinValue;
+
+            for (int row = 0; row < size[0] - 1; row++)
+            {
+
+                for (int col = 0; col < size[1] - 1; col++)
+                {
+                    var currentSum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+
+                        maxRow = row;
+                        maxCol = col;
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxRow, maxCol]} {matrix[maxRow, maxCol + 1]}");
+            Console.WriteLine($"{matrix[maxRow + 1, maxCol]} {matrix[maxRow + 1, maxCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
---
+++ C# Advanced - May 2022/P01_Sum_Matrix_Elements/P06_Jagged_Array_Modification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P06_Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
