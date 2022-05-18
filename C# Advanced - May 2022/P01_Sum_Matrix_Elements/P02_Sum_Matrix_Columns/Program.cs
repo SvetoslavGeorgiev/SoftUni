c1fe963fbea9024c8b6889f0b74d7551c3e083cf
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-18 16:55:34 +0100

    solution P02_Sum_Matrix_Columns

---
+++ C# Advanced - May 2022/P01_Sum_Matrix_Elements/P02_Sum_Matrix_Columns/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int row = 0; row < size[0]; row++)
+            {
+                var element = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = element.Dequeue();
+                }
+            }
+            for (int col = 0; col < size[1]; col++)
+            {
+                var sum = 0;
+                for (int row = 0; row < size[0]; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
