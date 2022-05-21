ccef4ca8aa7371123f48b08aec1fb02056ebf280
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-21 15:54:12 +0100

    added solution P03_Maximal_Sum

---
+++ C# Advanced - May 2022/S04_Multidimensional_Arrays_Exercise/P03_Maximal_Sum/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToArray();
+
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+
+            for (int row = 0; row < matrixSize[0]; row++)
+            {
+                var command = new Queue<int>(Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray());
+
+                for (int col = 0; col < matrixSize[1]; col++)
+                {
+
+                    matrix[row, col] = command.Dequeue();
+                }
+            }
+
+
+            var sum = int.MinValue;
+
+            var rowIndex = 0;
+            var colIndex = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    var currentSum = 0;
+
+
+                    for (int r = row; r <= row + 2; r++)
+                    {
+                        for (int c = col; c <= col + 2; c++)
+                        {
+                            currentSum += matrix[r, c];
+                        }
+                    }
+
+                    if (currentSum > sum)
+                    {
+                        sum = currentSum;
+                        rowIndex = row;
+                        colIndex = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {sum}");
+
+            for (int row = rowIndex; row < rowIndex + 3; row++)
+            {
+                for (int col = colIndex; col < colIndex + 3; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
