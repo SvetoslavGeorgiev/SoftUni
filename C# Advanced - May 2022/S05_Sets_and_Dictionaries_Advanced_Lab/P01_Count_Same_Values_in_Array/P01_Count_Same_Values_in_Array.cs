9d67d5649e9d5bfaafbc59450aa044f387cec8f3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 14:57:38 +0100

    adding solution - S05_Sets_and_Dictionaries_Advanced_Lab and first project in it - P01_Count_Same_Values_in_Array

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P01_Count_Same_Values_in_Array/P01_Count_Same_Values_in_Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Count_Same_Values_in_Array
+{
+    class P01_Count_Same_Values_in_Array
+    {
+        static void Main()
+        {
+            
+            var dic = new Dictionary<double, int>();
+
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (!dic.ContainsKey(input[i]))
+                {
+                    dic.Add(input[i], 0);
+                }
+                dic[input[i]]++;
+            }
+
+            foreach (var kvp in dic)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+
+        }
+    }
+}
