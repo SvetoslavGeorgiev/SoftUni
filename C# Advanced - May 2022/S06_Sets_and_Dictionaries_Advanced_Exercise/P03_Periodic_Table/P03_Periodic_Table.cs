d70ac9c2d4e6cb7eb129912dca7c6e9837418e49
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 17:55:20 +0100

    added project P03_Periodic_Table

---
+++ C# Advanced - May 2022/S06_Sets_and_Dictionaries_Advanced_Exercise/P03_Periodic_Table/P03_Periodic_Table.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_Periodic_Table
+{
+    class P03_Periodic_Table
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            var periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (var element in command)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            foreach (var element in periodicTable)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
