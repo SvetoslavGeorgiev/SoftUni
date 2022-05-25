f84cd77b7b2b6c7cfd992b361af88c27553b4d64
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 16:45:22 +0100

    adding project - P06_Record_Unique_Names

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P06_Record_Unique_Names/P06_Record_Unique_Names.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06_Record_Unique_Names
+{
+    class P06_Record_Unique_Names
+    {
+        static void Main()
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            var entries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entries; i++)
+            {
+                var entry = Console.ReadLine();
+
+                names.Add(entry);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
