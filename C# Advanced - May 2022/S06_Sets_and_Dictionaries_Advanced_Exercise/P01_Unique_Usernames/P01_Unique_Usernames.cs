0b2517ef2f79907a0a20e41109162bee3439d95b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 17:45:50 +0100

    created new solution - S06_Sets_and_Dictionaries_Advanced_Exercise and first project - P01_Unique_Usernames in it

---
+++ C# Advanced - May 2022/S06_Sets_and_Dictionaries_Advanced_Exercise/P01_Unique_Usernames/P01_Unique_Usernames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_Unique_Usernames
+{
+    class P01_Unique_Usernames
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            var names = new HashSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+
+                var currentName = Console.ReadLine();
+
+                names.Add(currentName);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
