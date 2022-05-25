73656b3df235e768f660ed72cefec39b71e8cd2c
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 16:36:46 +0100

    adding project - P04_Product_Shop

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P04_Product_Shop/P04_Product_Shop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Product_Shop
+{
+    class P04_Product_Shop
+    {
+        static void Main()
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Revision")
+            {
+
+                var token = command.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var shop = token[0];
+                var product = token[1];
+                var price = double.Parse(token[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+
+                }
+                shops[shop].Add(product, price);
+
+            }
+
+            var orderedShops = shops.OrderBy(s => s.Key).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var shop in orderedShops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var kvp in shop.Value)
+                {
+                    Console.WriteLine($"Product: {kvp.Key}, Price: {kvp.Value}");
+                }
+            }
+        }
+    }
+}
