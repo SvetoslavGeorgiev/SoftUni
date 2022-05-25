908682d4443fc53617e2b1bfd030b7c978512ca0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 18:00:21 +0100

    added project P05_Count_Symbols

---
+++ C# Advanced - May 2022/S06_Sets_and_Dictionaries_Advanced_Exercise/P05_Count_Symbols/P05_Count_Symbols.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05_Count_Symbols
+{
+    class P05_Count_Symbols
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            var dict = new SortedDictionary<char, int>();
+
+            foreach (var item in input)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+                dict[item]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
