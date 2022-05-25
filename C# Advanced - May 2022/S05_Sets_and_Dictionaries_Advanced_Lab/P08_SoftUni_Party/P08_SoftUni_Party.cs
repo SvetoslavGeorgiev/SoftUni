5068f2b50c48dfe260ca7734f5af8f9a2adc9d8b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 17:39:40 +0100

    adding project in the solution

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P08_SoftUni_Party/P08_SoftUni_Party.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08_SoftUni_Party
+{
+    class P08_SoftUni_Party
+    {
+        static void Main()
+        {
+            var vipMembers = new HashSet<string>();
+            var members = new HashSet<string>();
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(command[0]))
+                {
+                    vipMembers.Add(command);
+                }
+                else
+                {
+                    members.Add(command);
+                }
+            }
+
+            while (command != "END")
+            {
+
+                vipMembers.Remove(command);
+                members.Remove(command);
+                command = Console.ReadLine();
+            }
+
+            var notCome = vipMembers.Count + members.Count;
+
+            Console.WriteLine(notCome);
+            foreach (var vipMember in vipMembers)
+            {
+                Console.WriteLine(vipMember);
+            }
+            foreach (var member in members)
+            {
+                Console.WriteLine(member);
+            }
+        }
+    }
+}
