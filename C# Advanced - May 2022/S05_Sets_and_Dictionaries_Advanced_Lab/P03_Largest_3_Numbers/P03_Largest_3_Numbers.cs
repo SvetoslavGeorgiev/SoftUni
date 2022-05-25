13d9cd6f935c5cbdb4b43c544b2212dccd6f7e67
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 16:30:24 +0100

    adding project - P03_Largest_3_Numbers

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P03_Largest_3_Numbers/P03_Largest_3_Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Largest_3_Numbers
+{
+    class P03_Largest_3_Numbers
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).ToList();
+
+            var count = numbers.Count() >= 3 ? 3 : numbers.Count();
+
+            for (int i = 0; i < count; i++)
+                Console.Write($"{numbers[i]} ");
+
+
+        }
+    }
+}
