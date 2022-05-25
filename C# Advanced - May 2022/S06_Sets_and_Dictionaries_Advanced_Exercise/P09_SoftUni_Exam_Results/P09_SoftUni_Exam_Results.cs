3aead1dcd36e86af99ac04dafe1865ac5e882bab
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 18:59:17 +0100

    added project P09_SoftUni_Exam_Results

---
+++ C# Advanced - May 2022/S06_Sets_and_Dictionaries_Advanced_Exercise/P09_SoftUni_Exam_Results/P09_SoftUni_Exam_Results.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09_SoftUni_Exam_Results
+{
+    class P09_SoftUni_Exam_Results
+    {
+        static void Main()
+        {
+            var studentAndPoints = new Dictionary<string, int>();
+            var courseAndSubmissions = new Dictionary<string, int>();
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "exam finished")
+            {
+
+                var currentSubmission = command.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+                var student = currentSubmission[0];
+                if (!currentSubmission.Contains("banned"))
+                {
+
+
+                    var course = currentSubmission[1];
+                    var points = int.Parse(currentSubmission[2]);
+
+
+                    if (studentAndPoints.ContainsKey(student))
+                    {
+                        if (points > studentAndPoints[student])
+                        {
+                            studentAndPoints[student] = points;
+                        }
+                    }
+                    else
+                    {
+                        studentAndPoints.Add(student, points);
+                    }
+
+                    if (!courseAndSubmissions.ContainsKey(course))
+                    {
+                        courseAndSubmissions.Add(course, 0);
+                    }
+                    courseAndSubmissions[course]++;
+                }
+                else
+                {
+                    studentAndPoints.Remove(student);
+                }
+                
+            }
+            Console.WriteLine("Results: ");
+
+            foreach (var student in studentAndPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{student.Key} | {student.Value}");
+            }
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var submission in courseAndSubmissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{submission.Key} - {submission.Value}");
+            }
+        }
+    }
+}
