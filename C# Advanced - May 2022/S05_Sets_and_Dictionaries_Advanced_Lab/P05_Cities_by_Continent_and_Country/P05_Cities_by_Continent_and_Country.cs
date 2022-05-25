832a33e3fcbea6bcac8130c4818733285172759d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-25 16:39:46 +0100

    adding project - P05_Cities_by_Continent_and_Country

---
+++ C# Advanced - May 2022/S05_Sets_and_Dictionaries_Advanced_Lab/P05_Cities_by_Continent_and_Country/P05_Cities_by_Continent_and_Country.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05_Cities_by_Continent_and_Country
+{
+    class P05_Cities_by_Continent_and_Country
+    {
+        static void Main()
+        {
+            var entries = int.Parse(Console.ReadLine());
+
+            var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < entries; i++)
+            {
+                var entry = Console.ReadLine().Split();
+
+                var continent = entry[0];
+                var country = entry[1];
+                var city = entry[2];
+
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+                continents[continent][country].Add(city);
+            }
+
+            foreach (var continentKVP in continents)
+            {
+                var continent = continentKVP.Key;
+                Console.WriteLine($"{continent}:");
+
+                foreach (var countryKVP in continentKVP.Value)
+                {
+                    var country = countryKVP.Key;
+                    var cities = countryKVP.Value;
+                    Console.Write($"  {country} -> ");
+                    for (int i = 0; i < cities.Count; i++)
+                    {
+                        if (i != cities.Count - 1)
+                        {
+                            Console.Write($"{cities[i]}, ");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{cities[i]}");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
