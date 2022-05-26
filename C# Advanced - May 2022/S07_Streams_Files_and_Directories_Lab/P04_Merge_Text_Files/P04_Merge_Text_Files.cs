43e84094d1ce2de2c15694a954f51b4c8e35270e
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-26 20:54:00 +0100

    adding one new project and few files to others

---
+++ C# Advanced - May 2022/S07_Streams_Files_and_Directories_Lab/P04_Merge_Text_Files/P04_Merge_Text_Files.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+
+            string firstInputFilePath = @"../../../input1.txt";
+            string secondInputFilePath = @"../../../input2.txt";
+            string outputFilePath = @"../../../output.txt";
+
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+
+
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+
+            using (var reader = new StreamReader(firstInputFilePath))
+            {
+                using (var reader2 = new StreamReader(secondInputFilePath))
+                {
+                    using (var writer = new StreamWriter(outputFilePath))
+                    {
+                        while (true)
+                        {
+                            var line = reader.ReadLine();
+                            var line2 = reader2.ReadLine();
+
+
+                            if (line != null)
+                            {
+                                writer.WriteLine(line);
+                            }
+
+                            if (line2 != null)
+                            {
+                                writer.WriteLine(line2);
+                            }
+                            if (line == null && line2 == null)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
