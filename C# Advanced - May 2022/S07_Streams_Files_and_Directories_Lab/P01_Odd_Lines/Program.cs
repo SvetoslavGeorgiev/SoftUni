dc501030d517be854eb0549bff0dc7a6974320ac
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-26 17:51:17 +0100

    added new solution and the first project in it

---
+++ C# Advanced - May 2022/S07_Streams_Files_and_Directories_Lab/P01_Odd_Lines/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+
+        }
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                var counter = 0;
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+
+
+                    while (true)
+                    {
+
+                        string line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            return;
+                        }
+
+
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+
+    }
+}
