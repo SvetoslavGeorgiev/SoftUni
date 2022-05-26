e023833c8a40919c185948784568ffa37469bb98
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-26 18:02:39 +0100

    added new project P02_Line_Numbers

---
+++ C# Advanced - May 2022/S07_Streams_Files_and_Directories_Lab/P02_Line_Numbers/P02_Line_Numbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                var counter = 1;
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+
+
+                    while (true)
+                    {
+
+                        string line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            return;
+                        }
+
+
+                        writer.WriteLine($"{counter}. {line}");
+
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
