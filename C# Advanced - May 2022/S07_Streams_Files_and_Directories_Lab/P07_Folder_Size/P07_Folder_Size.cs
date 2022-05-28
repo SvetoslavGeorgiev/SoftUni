2f261f3cceb5aa4b4a91eea947eebde08ac4447d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-28 18:55:18 +0100

    added last project in the solution

---
+++ C# Advanced - May 2022/S07_Streams_Files_and_Directories_Lab/P07_Folder_Size/P07_Folder_Size.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"../../../TestFolder";
+
+            string outputFilePath = @"../../../output.txt";
+
+
+            GetFolderSize(folderPath, outputFilePath);
+
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            var dir = new DirectoryInfo(folderPath);
+
+            double sum = 0;
+
+            foreach (var file in dir.GetFiles())
+            {
+                sum += file.Length;
+            }
+            foreach (var dir1 in dir.GetDirectories()) 
+            {
+                foreach (var item in dir1.GetFiles())
+                {
+                    sum += item.Length;
+                }
+
+            }   
+            var sumInKb = sum / 1024;
+
+            
+
+
+            File.WriteAllText(outputFilePath, $"{sumInKb} KB");
+
+
+
+
+        }
+
+    }
+}
