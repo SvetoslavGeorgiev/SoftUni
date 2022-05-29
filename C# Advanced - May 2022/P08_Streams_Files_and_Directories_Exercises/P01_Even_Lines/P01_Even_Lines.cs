796d3ca01a16089fd258b09ad1ad09f29e6cd238
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-29 18:16:36 +0100

    new solution and new project in it

---
+++ C# Advanced - May 2022/P08_Streams_Files_and_Directories_Exercises/P01_Even_Lines/P01_Even_Lines.cs
@@ -0,0 +1,80 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            ProcessLines(inputFilePath); 
+
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            //var lines = new List<string>();
+            char[] charArr = new char[] { '-', ',', '.', '!', '?' };
+
+            using (var reader = new StreamReader(inputFilePath))
+            {
+
+                var counter = 0;
+
+                var line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    var list = new List<string>();
+
+                    if (counter % 2 == 0)
+                    {
+                        list = line.Split(' ').ToList();
+
+                        for (   int i = 0; i < list.Count; i++)
+                        {
+                            foreach (var ch in charArr)
+                            {
+                                if (list[i].Contains(ch))
+                                {
+
+                                    var word = ChangeCharInString(list[i], charArr);
+
+                                    list[i] = word;
+
+                                }
+                            }
+                        }
+
+                        list.Reverse();
+
+                        //lines.Add(string.Join(" ", list));
+                        Console.WriteLine(string.Join(" ", list));
+                    }
+                    counter++;
+                }
+            }
+            return string.Empty;
+            //return string.Join("\r\n", lines);
+        }
+
+        public static string ChangeCharInString(string word, char[] charArr)
+        {
+
+
+            foreach (var item in word)
+            {
+                if (charArr.Contains(item))
+                {
+                    word = word.Replace(item, '@');
+                }
+            }
+
+            return word;
+        }
+    }
+}
