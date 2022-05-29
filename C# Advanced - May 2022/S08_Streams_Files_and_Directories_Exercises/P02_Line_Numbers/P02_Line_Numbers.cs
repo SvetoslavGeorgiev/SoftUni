eec37ee9e4e1d21ce2fe359490d3893e70eda707
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-29 18:33:40 +0100

    updating last commit and adding new project - P02_Line_Numbers

---
+++ C# Advanced - May 2022/S08_Streams_Files_and_Directories_Exercises/P02_Line_Numbers/P02_Line_Numbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var counter = 0;
+
+            var chars = new char[] { '-', ',', '.', '!', '?' };
+
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                var currentLine = string.Empty;
+
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        counter++;
+                        var allChars = 0;
+                        var symbolCount = 0;
+
+                        foreach (var @char in currentLine)
+                        {
+                            if (char.IsLetterOrDigit(@char))
+                            {
+                                allChars++;
+                            }
+                            else if (char.IsPunctuation(@char))
+                            {
+                                symbolCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {currentLine} ({allChars})({symbolCount})");
+                    }
+                }
+            }
+        }
+    }
+}
