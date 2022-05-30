aa322d5ddbf43e776661e3d9ad08e94a5a9ec414
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-30 13:51:17 +0100

    project - P06_Zip_and_Extracts added

---
+++ C# Advanced - May 2022/S08_Streams_Files_and_Directories_Exercises/P06_Zip_and_Extracts/P06_Zip_and_Extracts.cs
@@ -0,0 +1,40 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\fileToArchive";
+            string zipArchiveFilePath = @"..\..\..\archive.zip";
+            string outputFilePath = @"..\..\..\extracted.png";
+
+            var fileName = "copyMe.png";
+
+
+            ZipFileToArchive(inputFilePath, zipArchiveFilePath);
+            ExtractFileFromArchive(zipArchiveFilePath, fileName, outputFilePath);
+
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            
+            ZipFile.CreateFromDirectory(inputFilePath, zipArchiveFilePath);
+
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+
+            //ZipFile.ExtractToDirectory(zipArchiveFilePath, outputFilePath, true);
+
+            ZipFile.OpenRead(zipArchiveFilePath).GetEntry(fileName).ExtractToFile(outputFilePath);
+
+
+        }
+    }
+}
