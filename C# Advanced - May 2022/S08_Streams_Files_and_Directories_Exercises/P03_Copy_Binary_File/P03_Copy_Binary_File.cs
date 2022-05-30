87170579b51f0f4a3625024fcd24e7a2df101370
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-05-30 11:35:58 +0100

    new project added P03_Copy_Binary_File

---
+++ C# Advanced - May 2022/S08_Streams_Files_and_Directories_Exercises/P03_Copy_Binary_File/P03_Copy_Binary_File.cs
@@ -0,0 +1,42 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (var writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+
+                    while (true)
+                    {
+                        var buffer = new byte[4096];
+
+                        var total = reader.Read(buffer, 0, buffer.Length);
+
+                        if (total == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, total);
+
+                    }
+                }
+
+            }
+        }
+    }
+}
