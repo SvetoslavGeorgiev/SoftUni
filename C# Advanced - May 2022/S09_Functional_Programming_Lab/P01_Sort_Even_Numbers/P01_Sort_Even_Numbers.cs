056fdecdfdddf4a6ab6a67ff0fa25973d4408229
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 06:40:08 +0100

    new solution added and first project - P01_Sort_Even_Numbers in it

---
+++ C# Advanced - May 2022/S09_Functional_Programming_Lab/P01_Sort_Even_Numbers/P01_Sort_Even_Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_Sort_Even_Numbers
+{
+    public class P01_Sort_Even_Numbers
+    {
+        static void Main()
+        {
+
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
