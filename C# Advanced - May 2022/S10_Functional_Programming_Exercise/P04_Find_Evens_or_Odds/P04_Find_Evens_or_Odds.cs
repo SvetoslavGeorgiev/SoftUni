6830db854807ac3c7c3ff0740e3013ecebb3cced
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:29:37 +0100

    projectr P04_Find_Evens_or_Odds added

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P04_Find_Evens_or_Odds/P04_Find_Evens_or_Odds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Find_Evens_or_Odds
+{
+    public class P04_Find_Evens_or_Odds
+    {
+        static void Main()
+        {
+            var range = Console.ReadLine()
+                .Split()
+                .Select(int.Parse).
+                ToList();
+
+            var list = new List<int>();
+
+            var condition = Console.ReadLine();
+
+            Predicate<int> pr = Cheker(condition);
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (pr(i))
+                {
+                    list.Add(i);
+                }
+            }
+            Console.Write(string.Join(" ", list));
+        }
+        private static Predicate<int> Cheker(string condition)
+        {
+            switch (condition)
+            {
+                case "odd":
+                    return x => x % 2 != 0;
+                case "even":
+                    return x => x % 2 == 0;
+                default:
+                    return null;
+            }
+        }
+    }
+}
