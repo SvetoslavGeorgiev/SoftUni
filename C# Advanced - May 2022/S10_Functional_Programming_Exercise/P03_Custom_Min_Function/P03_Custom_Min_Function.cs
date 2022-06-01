18c1ce606e0aa7c699d6f03619147f65bcd395a3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:24:46 +0100

    projectr P03_Custom_Min_Function added

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P03_Custom_Min_Function/P03_Custom_Min_Function.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_Custom_Min_Function
+{
+    public class P03_Custom_Min_Function
+    {
+        static void Main()
+        {
+            var input = Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            Func<List<int>, int> checker = x => x.Min();
+
+            var number = checker(input);
+
+            Console.WriteLine(number);
+        }
+    }
+}
