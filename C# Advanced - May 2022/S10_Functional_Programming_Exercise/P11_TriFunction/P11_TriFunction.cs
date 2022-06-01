c14b4d6f9cd68fc20afbcf3118bd87f7fe9dc139
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 09:04:08 +0100

    added last project into the solution

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P11_TriFunction/P11_TriFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P11_TriFunction
+{
+    public class P11_TriFunction
+    {
+        static void Main()
+        {
+
+            var number = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> func = (x, y) =>
+            {
+                int sum = 0;
+                foreach (var item in x)
+                {
+                    sum += item;
+                    if (sum >= y)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            var name = names.Where(x => func(x, number));
+
+            Console.WriteLine(string.Join(" ", name));
+        }
+    }
+}
