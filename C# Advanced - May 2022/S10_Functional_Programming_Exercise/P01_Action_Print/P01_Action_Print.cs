7831da7b5b0b7c55d897b8e31f0f8b948944da56
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:15:04 +0100

    added new solution and first project in it - P01_Action_Print

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P01_Action_Print/P01_Action_Print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Action_Print
+{
+    public class P01_Action_Print
+    {
+        static void Main()
+        {
+            var input = Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            
+
+            Action<string> print = Print(input);
+
+            input.ForEach(print);
+        }
+
+        private static Action<string> Print(List<string> input)
+        {
+            return name => Console.WriteLine(name);
+        }
+    }
+}
