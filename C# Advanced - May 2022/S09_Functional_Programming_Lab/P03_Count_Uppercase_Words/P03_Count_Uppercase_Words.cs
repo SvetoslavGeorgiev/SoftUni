e489b8797832b3fbaeae1fc35ec1af7f78a7aa20
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 06:51:59 +0100

    P03_Count_Uppercase_Words added

---
+++ C# Advanced - May 2022/S09_Functional_Programming_Lab/P03_Count_Uppercase_Words/P03_Count_Uppercase_Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Count_Uppercase_Words
+{
+    public class P03_Count_Uppercase_Words
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x[0] == x.ToUpper()[0])
+                .ToList();
+
+
+            Console.WriteLine(string.Join("\n", input));
+        }
+    }
+}
