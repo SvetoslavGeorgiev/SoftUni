5bcc576f74ec256bbd30f8b930ed02cdfd765ee2
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 06:58:32 +0100

    P04_Add_VAT added

---
+++ C# Advanced - May 2022/S09_Functional_Programming_Lab/P04_Add_VAT/P04_Add_VAT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_Add_VAT
+{
+    public class P04_Add_VAT
+    {
+        static void Main()
+        {
+            var priceWithVat = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", priceWithVat.Select(x => $"{x:F2}"))); ;
+        }
+    }
+}
