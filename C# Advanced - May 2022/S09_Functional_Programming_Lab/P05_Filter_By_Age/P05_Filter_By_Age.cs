ec3296ee0daf5e5047215bd83e8cbe9015a44fb6
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:07:10 +0100

    last project added in that solution

---
+++ C# Advanced - May 2022/S09_Functional_Programming_Lab/P05_Filter_By_Age/P05_Filter_By_Age.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P05_Filter_By_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
