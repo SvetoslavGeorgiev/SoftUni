462f020d01b1e951b1b61141c36775e540a3f8c7
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:21:17 +0100

    projectr P02_Knights_of_Honor added

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P02_Knights_of_Honor/P02_Knights_of_Honor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_Knights_of_Honor
+{
+    public class P02_Knights_of_Honor
+    {
+        static void Main()
+        {
+            var input = Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            Action<string> print = Print(input);
+
+            input.ForEach(print);
+        }
+
+        private static Action<string> Print(List<string> input)
+        {
+            return name => Console.WriteLine($"Sir {name}");
+        }
+    }
+}
