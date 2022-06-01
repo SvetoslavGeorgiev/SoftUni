2c9b3f160d20d4f98358741c37e0af98973d8bc9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 07:43:48 +0100

    added new project - P07_Predicate_For_Names

---
+++ C# Advanced - May 2022/S10_Functional_Programming_Exercise/P07_Predicate_For_Names/P07_Predicate_For_Names.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_Predicate_For_Names
+{
+    public class P07_Predicate_For_Names
+    {
+        static void Main()
+        {
+            var requiredLength = int.Parse(Console.ReadLine());
+
+            Func<string, bool> maxLenght = MaxLennght(requiredLength);
+
+            Console
+                .ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(maxLenght)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        private static Func<string, bool> MaxLennght(int requiredLength)
+        {
+            return x => x.Length <= requiredLength;
+        }
+    }
+}
