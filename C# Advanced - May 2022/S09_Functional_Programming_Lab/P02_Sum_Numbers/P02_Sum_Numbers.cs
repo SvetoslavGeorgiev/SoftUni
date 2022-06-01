76db637fc97d8b2aff5a9d9d5c7007a10549b7a0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-01 06:47:01 +0100

    P02_Sum_Numbers added

---
+++ C# Advanced - May 2022/S09_Functional_Programming_Lab/P02_Sum_Numbers/P02_Sum_Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_Sum_Numbers
+{
+    public class P02_Sum_Numbers
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
