10ad2918c1da322db1ac8592fe1c61c83dd20510
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 17:06:54 +0100

    new project P03_Car_Constructors added

---
+++ C# Advanced - May 2022/S11_Defining_Classes_Lab/P03_Car_Constructors/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+        : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+
+        private string make;
+        private string model;
+        private int year;
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+    }
+}
