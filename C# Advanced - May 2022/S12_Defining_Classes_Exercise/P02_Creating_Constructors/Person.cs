1259b5cca4ef677383ddbf1fd77584525b05893b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 20:04:43 +0100

    new project added

---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P02_Creating_Constructors/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Person(int age) : this("No name", age)
+        {
+        }
+
+        public Person() : this("No name", 1)
+        {
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P02_Creating_Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
