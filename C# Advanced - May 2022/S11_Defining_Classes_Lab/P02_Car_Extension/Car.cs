1f8a1293770f8a1906a8d773127538cca3b07998
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 12:09:57 +0100

    project P02_Car_Extension

---
+++ C# Advanced - May 2022/S11_Defining_Classes_Lab/P02_Car_Extension/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            bool canContinue = this.FuelQuantity - (distance * this.FuelConsumption) > 0;
+
+            if (canContinue)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+
+
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+    }
+}
