280655b81bb873b87918ca14f6394e77849ff901
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 19:43:22 +0100

    new solution and first project - P01_Define_a_Class_Person in it

---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P01_Define_a_Class_Person/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+    }
+}
