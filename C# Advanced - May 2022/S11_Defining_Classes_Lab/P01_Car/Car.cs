26d2c9092fe6d95063ba8a4a27337103675d4840
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 11:21:13 +0100

    new solution and first project in it

---
+++ C# Advanced - May 2022/S11_Defining_Classes_Lab/P01_Car/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
