c926ad45b0f7eb86a7cd1eff5f78871875d23a76
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-02 20:23:09 +0100

    new project added

---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P03_Oldest_Family_Member/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        public Family(List<Person> familyMemvers)
+        {
+            FamilyMembers = familyMemvers;
+        }
+
+
+        private List<Person> familyMembers;
+
+        public List<Person> FamilyMembers { get; set; }
+
+        public void AddMember(Person newMember)
+        {
+            FamilyMembers.Add(newMember);
+        }
+        public Person GetOldestMember()
+        {
+            return  FamilyMembers.OrderByDescending(x => x.Age).FirstOrDefault();
+
+        }
+
+    }
+}
---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P03_Oldest_Family_Member/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+
+            var newMember = new List<string>();
+
+            var family = new Family();
+
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+
+                newMember = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                var name = newMember[0];
+                var age = int.Parse(newMember[1]);
+
+                var familyMember = new Person(name, age);
+
+                family.AddMember(familyMember);
+            }
+
+            var theOldestOneInTheFamily = family.GetOldestMember();
+
+            Console.WriteLine($"{theOldestOneInTheFamily.Name} {theOldestOneInTheFamily.Age}");
+        }
+    }
+}
