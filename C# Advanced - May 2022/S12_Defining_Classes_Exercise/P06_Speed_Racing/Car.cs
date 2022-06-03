15b0a8792f6c2ac9646ffaa7a2767a048c17ab6d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-03 21:18:42 +0100

    new project P06_Speed_Racing added

---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P06_Speed_Racing/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car()
+        {
+            TravelledDistance = 0;
+        }
+
+        public Car(string make, double fuelQuantity, double fuelConsumptionPerKilometer)
+        : this()
+        {
+            Make = make;
+            FuelQuantity = fuelQuantity;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = 0;
+        }
+
+        
+
+
+        public string Make { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double distance)
+        {
+            var fuelneeded = distance * FuelConsumptionPerKilometer ;
+
+            if (fuelneeded <= FuelQuantity)
+            {
+                FuelQuantity -= fuelneeded;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public string WhoAmI()
+        {
+
+            var result = $"{Make} {FuelQuantity:F2} {TravelledDistance}";
+
+            return result;
+        }
+
+    }
+}
---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P06_Speed_Racing/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            var numberOfCars = int.Parse(Console.ReadLine());
+            var carList = new List<Car>();
+            for (int i = 0; i < numberOfCars; i++)
+            {
+
+
+                var carCharacteristics = Console.ReadLine().Split();
+
+                var make = carCharacteristics[0];
+                var fuelQuantity = double.Parse(carCharacteristics[1]);
+                var fuelConsumptionPerKilometer = double.Parse(carCharacteristics[2]);
+
+
+                var car = new Car(make, fuelQuantity, fuelConsumptionPerKilometer);
+
+                carList.Add(car);
+            }
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+
+                var tokens  = command.Split();
+
+                var make = tokens[1];
+                var distance = double.Parse(tokens[2]);
+
+                foreach (var car in carList)
+                {
+                    if (make == car.Make)
+                    {
+                        car.Drive(distance);
+                    }
+                }
+            }
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car.WhoAmI());
+            }
+        }
+    }
+}
