074a5a0c8a6299471dd3cbef41630add691eaa24
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-04 04:13:52 +0100

    project P10_SoftUni_Parking added

---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P10_SoftUni_Parking/Parking.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public Parking(int capacity)
+        {
+            Cars = new List<Car> ();
+            Capacity = capacity;
+        }
+
+        private List<Car> cars;
+        private int capacity;
+
+
+        public int Count { get { return Cars.Count; } }
+
+        public List<Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+
+        public string AddCar(Car car)
+        {
+            if (Cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists";
+
+            }
+            else if (Capacity <= Cars.Count)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                Cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+
+            return Cars.Find(x => x.RegistrationNumber == registrationNumber);
+
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (Cars.Find(x => x.RegistrationNumber == registrationNumber) == null)
+            {
+
+                return "Car with that registration number, doesn't exist!";
+
+            }
+            else
+            {
+                Cars.Remove(GetCar(registrationNumber));
+                return $"Successfully removed {registrationNumber}";
+            }
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var reg in registrationNumbers)
+            {
+                RemoveCar(reg);
+            }
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S12_Defining_Classes_Exercise/P10_SoftUni_Parking/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+            var car3 = new Car("Audi", "A3", 110, "EB4787MN");
+            var car4 = new Car("Audi", "A3", 110, "EB2787MN");
+            var car5 = new Car("Audi", "A3", 110, "EB6787MN");
+            var car6 = new Car("Audi", "A3", 110, "EB4787MN");
+
+
+            var regList = new List<string>();
+            regList.Add("EB4787MN");
+            regList.Add("EB8787MN");
+            regList.Add("EB2787MN");
+            regList.Add("EBdsasfsaN");
+
+            Console.WriteLine(car.ToString());
+            // Make: Skoda
+            // Model: Fabia
+            // HorsePower: 65
+            // RegistrationNumber: CC1856BG
+
+            var parking = new Parking(5);
+            Console.WriteLine(parking.AddCar(car));
+            // Successfully added new car Skoda CC1856BG
+
+            Console.WriteLine(parking.AddCar(car));
+            // Car with that registration number, already exists!
+
+            Console.WriteLine(parking.AddCar(car2));
+            // Successfully added new car Audi EB8787MN
+            Console.WriteLine(parking.AddCar(car3));
+            Console.WriteLine(parking.AddCar(car4));
+            Console.WriteLine(parking.AddCar(car5));
+            Console.WriteLine(parking.AddCar(car6));
+            Console.WriteLine(parking.GetCar("EB8787MN").ToString());
+            // Make: Audi
+            // Model: A3
+            // HorsePower: 110
+            // RegistrationNumber: EB8787MN
+            parking.RemoveSetOfRegistrationNumber(regList);
+            Console.WriteLine(parking.RemoveCar("EB8787MN"));
+            // Successfullyremoved EB8787MN
+
+            Console.WriteLine(parking.Count);
+            // 1
+
+        }
+    }
+}
