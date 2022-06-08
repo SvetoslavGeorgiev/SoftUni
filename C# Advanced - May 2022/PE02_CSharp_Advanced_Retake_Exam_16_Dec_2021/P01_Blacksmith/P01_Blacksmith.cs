e2ebffed4a6f912586259de017624d04fc01620d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-09 00:57:37 +0100

    added new solution and new project in it

--- C# Advanced - May 2022/PE02_CSharp_Advanced_Retake_Exam_16_Dec_2021/P01_Blacksmith/P01_Blacksmith.cs
+++ C# Advanced - May 2022/PE02_CSharp_Advanced_Retake_Exam_16_Dec_2021/P01_Blacksmith/P01_Blacksmith.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P01_Blacksmith
 {
@@ -6,7 +8,102 @@ namespace P01_Blacksmith
     {
         static void Main()
         {
-            
+            var steel = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            var carbon = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            var swords = new Dictionary<string, int>(); 
+
+            while (steel.Any() && carbon.Any())
+            {
+                var currentSteel = steel.Dequeue();
+                var currentCarbon = carbon.Pop();
+
+                var sword = GetSword(currentSteel + currentCarbon);
+
+                if (sword != null)
+                {
+                    if (swords.ContainsKey(sword))
+                    {
+                        swords[sword]++;
+                    }
+                    else
+                    {
+                        swords.Add(sword, 1);
+                    }
+                }
+                else
+                {
+                    currentCarbon += 5;
+                    carbon.Push(currentCarbon);
+                }
+            }
+
+            if (swords.Any())
+            {
+                Console.WriteLine($"You have forged {swords.Sum(x => x.Value)} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            if (steel.Any())
+            {
+                Console.WriteLine($"Steel left: { string.Join(", ", steel)}");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            if (carbon.Any())
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", carbon)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            foreach (var sword in swords.OrderBy(x => x.Key))
+            {
+
+                Console.WriteLine($"{sword.Key}: {sword.Value}");
+            }
+
+        }
+
+        private static string GetSword(int resource)
+        {
+            var sword = string.Empty;
+
+            switch (resource)
+            {
+                case 70:
+                    sword = "Gladius";
+                    break;
+                case 80:
+                    sword = "Shamshir";
+                    break;
+                case 90:
+                    sword = "Katana";
+                    break;
+                case 110:
+                    sword = "Sabre";
+                    break;
+                case 150:
+                    sword = "Broadsword";
+                    break;
+                default:
+                    sword = null;
+                    break;
+            }
+
+            return sword;
         }
     }
 }
