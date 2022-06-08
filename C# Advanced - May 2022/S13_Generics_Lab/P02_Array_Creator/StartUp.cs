1bfc914f79f5794c581678038de94a091d203416
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-08 18:38:46 +0100

    new project P02_Array_Creator added

---
+++ C# Advanced - May 2022/S13_Generics_Lab/P02_Array_Creator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+        }
+    }
+}
