e18d7091541ae1bd53c01564ef5e11135ca22092
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 21:54:33 +0100

    project P06_Generic_Count_Method_Double added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P06_Generic_Count_Method_Double/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Generic_Count_Method_Double
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<double>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<double>(list);
+            var elementToCompare = double.Parse(Console.ReadLine());
+
+            var count = box.CountOfBiggerElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
