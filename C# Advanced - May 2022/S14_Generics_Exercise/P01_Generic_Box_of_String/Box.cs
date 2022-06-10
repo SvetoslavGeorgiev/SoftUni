25151695e52ce66ea64a72275016d76c2a71bd65
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 17:07:44 +0100

    new solution started and added first project in it

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P01_Generic_Box_of_String/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_Generic_Box_of_String
+{
+    public class Box<T>
+    {
+
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public T Element { get;}
+
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P01_Generic_Box_of_String/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P01_Generic_Box_of_String
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+
+                Console.WriteLine(box);
+
+            }
+        }
+    }
+}
