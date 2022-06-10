32bb532a445e9110aa6c982cffbf3bc7bc0bf100
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 17:14:48 +0100

    new project P02_Generic_Box_of_Integer added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P02_Generic_Box_of_Integer/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public T Element { get;}
+
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P02_Generic_Box_of_Integer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P02_Generic_Box_of_Integer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+
+                Console.WriteLine(box);
+
+            }
+        }
+    }
+}
