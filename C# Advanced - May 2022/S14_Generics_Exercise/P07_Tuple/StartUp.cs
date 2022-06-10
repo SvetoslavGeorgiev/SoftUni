1856979c813aa542c4bdfd7a90992d5e26977af1
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 23:10:21 +0100

    project P07_Tuple added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P07_Tuple/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P07_Tuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split();
+
+            var fullName = $"{input[0]} {input[1]}";
+            var city = $"{input[2]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+
+            var name = nameAndBeer[0];
+            var litters = int.Parse(nameAndBeer[1]);
+
+
+            var intAndDouble = Console.ReadLine().Split();
+
+            var intNum = int.Parse(intAndDouble[0]);
+            var doubleNum = double.Parse(intAndDouble[1]);
+
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, city);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, litters);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNum, doubleNum);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P07_Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07_Tuple
+{
+    public class Tuple<Tfirst , Tsecond>
+    {
+        public Tuple(Tfirst first, Tsecond second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public Tfirst First { get; set; }
+        public Tsecond Second { get; set; }
+
+        public override string ToString()
+        {
+            return $"{First} -> {Second}";
+        }
+    }
+}
