bdc1a47d5e2363518b00a766731179d11d894e9c
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-11 00:37:20 +0100

    project P08_Threeuple added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P08_Threeuple/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P08_Threeuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split();
+
+            var fullName = $"{input[0]} {input[1]}";
+            var address = $"{input[2]}";
+            var city = input.Length > 4 ? $"{input[3]} {input[4]}" : $"{input[3]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+
+            var name = nameAndBeer[0];
+            var litters = int.Parse(nameAndBeer[1]);
+            var drunkOrNot = nameAndBeer[2] == "drunk" ? true : false;
+
+
+            var intAndDouble = Console.ReadLine().Split();
+
+            var name1 = intAndDouble[0];
+            var bankBalance = double.Parse(intAndDouble[1]);
+            var bankName = intAndDouble[2];
+
+
+            Threeuple<string, string, string> firstThreeuple = new Threeuple<string, string, string>(fullName, address, city);
+            Threeuple<string, int, bool> secondThreeuple = new Threeuple<string, int, bool>(name, litters, drunkOrNot);
+            Threeuple<string, double, string> thirdThreeuple = new Threeuple<string, double, string>(name1, bankBalance, bankName);
+
+            Console.WriteLine(firstThreeuple);
+            Console.WriteLine(secondThreeuple);
+            Console.WriteLine(thirdThreeuple);
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P08_Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08_Threeuple
+{
+    public class Threeuple<Tfirst , Tsecond, TThird>
+    {
+        public Threeuple(Tfirst first, Tsecond second, TThird third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        private Tsecond second;
+        private Tfirst first;
+        private TThird third;
+
+        public override string ToString()
+        {
+            return $"{first} -> {second} -> {third}";
+        }
+    }
+}
