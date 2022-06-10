7fdaf279a79553222d1ec88a198d11342115f53f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 18:46:38 +0100

    project P04_Generic_Swap_Method_Integer added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P04_Generic_Swap_Method_Integer/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04_Generic_Swap_Method_Integer
+{
+    public class Box<T>
+    {
+
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> itemList)
+        {
+            Elements = itemList;
+        }
+
+        public List<T> Elements { get;}
+        public T Element { get;}
+
+        public void Swap(List<T> elements, int firstOne, int secondOne)
+        {
+            var first = elements[firstOne];
+            elements[firstOne] = elements[secondOne];
+            elements[secondOne] = first;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P04_Generic_Swap_Method_Integer/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Generic_Swap_Method_Integer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                
+                list.Add(input);
+
+            }
+            var input1 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var box = new Box<int>(list);
+            box.Swap(list, input1[0], input1[1]);
+
+            Console.WriteLine(box);
+        }
+    }
+}
