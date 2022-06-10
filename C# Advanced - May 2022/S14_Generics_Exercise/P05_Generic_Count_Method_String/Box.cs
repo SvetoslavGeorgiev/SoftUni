505e2b5bc9d64ab8069dc687a0f0736ef526eec5
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-10 21:30:40 +0100

    project P05_Generic_Count_Method_String added

---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P05_Generic_Count_Method_String/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace P05_Generic_Count_Method_String
+{
+    public class Box<T> : IComparable<T> where T : IComparable<T>
+    {
+
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> itemList)
+        {
+            Elements = itemList;
+        }
+
+        public List<T> Elements { get;}
+        public T Element { get;}
+
+        public int CompareTo(T other)
+        {
+            return Element.CompareTo(other);
+        }
+
+        public int CountOfBiggerElements<T>(List<T> list, T line) where T : IComparable
+        {
+            return list.Count(word => word.CompareTo(line) > 0);
+        }
+
+        public void Swap(List<T> elements, int firstOne, int secondOne)
+        {
+            var first = elements[firstOne];
+            elements[firstOne] = elements[secondOne];
+            elements[secondOne] = first;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S14_Generics_Exercise/P05_Generic_Count_Method_String/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_Generic_Count_Method_String
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+
+            var count = box.CountOfBiggerElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
