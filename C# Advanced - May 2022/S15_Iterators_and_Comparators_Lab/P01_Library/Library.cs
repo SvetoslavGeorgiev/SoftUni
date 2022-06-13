59840741dce3bb8f4815f81987ff9522a4c3276b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-13 22:58:24 +0100

    new solution and first project added

---
+++ C# Advanced - May 2022/S15_Iterators_and_Comparators_Lab/P01_Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);                
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
