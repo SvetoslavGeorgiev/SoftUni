49cef42a0205978947b012317e202030099de4ad
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-13 23:32:55 +0100

    new project P02_LibraryIterator added

---
+++ C# Advanced - May 2022/S15_Iterators_and_Comparators_Lab/P02_LibraryIterator/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+    }
+}
