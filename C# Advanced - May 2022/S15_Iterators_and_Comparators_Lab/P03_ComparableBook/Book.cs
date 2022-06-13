5fd3b313a7a9d85a066781e094e2385ef3b1a7b0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-14 00:35:24 +0100

    project P03_ComparableBook added

---
+++ C# Advanced - May 2022/S15_Iterators_and_Comparators_Lab/P03_ComparableBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
