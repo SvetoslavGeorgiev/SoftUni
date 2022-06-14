4bb127b6445f3c1b7d55f44fcc744f686ba79f17
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-14 01:10:08 +0100

    last project in the solution added

---
+++ C# Advanced - May 2022/S15_Iterators_and_Comparators_Lab/P04_BookComparer/Library.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());                
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+
+            private readonly List<Book> books;
+
+            private int index;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+            public Book Current => this.books[this.index];
+
+            object IEnumerator.Current => Current;
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.index < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.index = -1;
+            }
+            
+        }
+    }
+}
