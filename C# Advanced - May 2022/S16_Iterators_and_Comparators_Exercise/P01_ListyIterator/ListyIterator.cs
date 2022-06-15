b52c5e9a6487685d1667522df34d3d605ca36b36
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-15 23:02:07 +0100

    new solution and first project in it

---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P01_ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_ListyIterator
+{
+    public class ListyIterator<T>
+    {
+
+        private List<T> list;
+        private int index;
+
+        public ListyIterator(params T[] data)
+        {
+            list = new List<T>(data);
+            this.index = 0;
+        }
+        
+        public bool HasNext() => index < list.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                this.index++;
+            }
+            return canMove;
+        }
+
+        public void Print() 
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{list[this.index]}");
+
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P01_ListyIterator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //ListyIterator<int> list = new ListyIterator<int>(1, 2, 3, 4, 5);
+
+            var input = string.Empty;
+
+            ListyIterator<string> list = null;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+
+                var tokens = input.Split();
+
+                if (tokens[0] == "Create")
+                {
+
+                    list = new ListyIterator<string>(tokens.Skip(1).ToArray());    
+
+
+                }
+                else if (tokens[0] == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+                else if (tokens[0] == "Print")
+                {
+                    list.Print();
+                }
+                else if (tokens[0] == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+
+            }
+
+
+        }
+    }
+}
