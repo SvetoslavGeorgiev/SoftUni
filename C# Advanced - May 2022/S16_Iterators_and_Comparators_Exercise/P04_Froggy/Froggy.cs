9aa3f89ebdf56d09bed9b4edc25fe36808dc539d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-16 00:47:21 +0100

    project - P04_Froggy added

---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P04_Froggy/Froggy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04_Froggy
+{
+    public class Froggy : IEnumerable<int>
+    {
+        private int[] lake;
+
+        public Froggy(params int[] rocks)
+        {
+            this.lake = rocks;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < lake.Length; i += 2)
+            {
+                yield return lake[i];
+            }
+
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P04_Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var lake = new Froggy(input);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
