ef9e2ce594107aa5bb810a36c2daff094e73b934
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-16 00:24:35 +0100

    project - P03_Stack added

---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P03_Stack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine().TrimEnd()) != "END")
+            {
+
+                var tokens = command.Split();
+
+                if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(x => x.Split(",").First()).ToArray());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P03_Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+
+        private List<T> list;
+
+        public Stack(params T[] data)
+        {
+            list = new List<T>(data);
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach (var item in elements)
+            {
+                list.Insert(list.Count, item);
+            }
+            
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            T ret = list[list.Count - 1];
+            this.list.RemoveAt(this.list.Count - 1);
+
+            return ret; 
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                yield return list[i];   
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
