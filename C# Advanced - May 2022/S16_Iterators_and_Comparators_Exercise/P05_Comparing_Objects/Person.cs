8c2049fe8ad9841f25d415014283f6a616e6f65a
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-16 01:38:10 +0100

    project - P05_Comparing_Objects added

---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P05_Comparing_Objects/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace P05_Comparing_Objects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+            Count = 0;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        private int Count { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+
+            if (result == 0)
+            {
+                result = Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P05_Comparing_Objects/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05_Comparing_Objects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var command = string.Empty;
+
+            var people = new List<Person>();
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var tokens = command.Split();
+
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+                var town = tokens[2];
+
+                var person = new Person(name, age, town);
+
+                people.Add(person);
+
+
+            }
+            var indexOfThePersonToCompare = int.Parse(Console.ReadLine()) - 1;
+
+            var equalPeople = 0;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+
+                var result = people[indexOfThePersonToCompare].CompareTo(people[i]);
+
+                if (result == 0)
+                {
+                    equalPeople++;
+                }
+
+            }
+
+            if (equalPeople > 1)
+            {
+                Console.WriteLine($"{equalPeople} {people.Count - equalPeople} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+            
+
+        }
+    }
+}
