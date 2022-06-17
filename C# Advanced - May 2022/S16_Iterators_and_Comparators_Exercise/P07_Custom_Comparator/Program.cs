9d7394ad0a6f4e54de6156cf8d625629e0afbd68
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-17 18:07:44 +0100

    added last project in that solution

---
+++ C# Advanced - May 2022/S16_Iterators_and_Comparators_Exercise/P07_Custom_Comparator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_Custom_Comparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers  = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+
+            Func<int, int, int> sortFunction = (x, y) => (x % 2 == 0 && y % 2 != 0) ? - 1 : (x % 2 != 0 && y % 2 == 0) ? 1 : x > y ? 1 : x < y ? -1 : 0;
+
+            numbers.Sort((x, y) => sortFunction(x, y));
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
