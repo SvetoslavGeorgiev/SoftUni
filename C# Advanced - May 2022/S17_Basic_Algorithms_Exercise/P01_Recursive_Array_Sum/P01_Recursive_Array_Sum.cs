0c17495ea2111d0b46277cddd3dcfc06b7e8dc93
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-26 22:52:28 +0100

    added new solution and first project - P01_Recursive_Array_Sum in it

---
+++ C# Advanced - May 2022/S17_Basic_Algorithms_Exercise/P01_Recursive_Array_Sum/P01_Recursive_Array_Sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace P01_Recursive_Array_Sum
+{
+    public class P01_Recursive_Array_Sum
+    {
+        static void Main()
+        {
+
+            var arr  = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            long sum = SumArray(arr, 0);
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static long SumArray(int[] arr, int startIndex)
+        {
+            if (startIndex == arr.Length - 1)
+            {
+                return arr[startIndex];
+            }
+
+            long sum = arr[startIndex] + SumArray(arr, startIndex + 1);
+
+            return sum;
+
+        }
+    }
+}
