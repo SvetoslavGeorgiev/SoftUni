b919694c88876b6e5b7befe54ab3762f5cad1f7f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-26 23:17:09 +0100

    new project P02_Recursive_Factorial added

---
+++ C# Advanced - May 2022/S17_Basic_Algorithms_Exercise/P02_Recursive_Factorial/P02_Recursive_Factorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P02_Recursive_Factorial
+{
+    public class P02_Recursive_Factorial
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+
+
+            long sum = SumArray(number);
+
+            Console.WriteLine(sum);
+        }
+
+
+        private static long SumArray(int startIndex)
+        {
+
+            if (startIndex == 1)
+            {
+                return startIndex;
+            }
+
+            return startIndex * SumArray(startIndex - 1);
+
+        }
+    }
+}
