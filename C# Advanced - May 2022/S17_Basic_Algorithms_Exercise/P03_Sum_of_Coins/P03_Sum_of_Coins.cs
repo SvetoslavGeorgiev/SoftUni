83d39367772433c1d87eb4dc8c610117a3c9cd65
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-28 17:55:20 +0100

    new project - P03_Sum_of_Coins added

---
+++ C# Advanced - May 2022/S17_Basic_Algorithms_Exercise/P03_Sum_of_Coins/P03_Sum_of_Coins.cs
@@ -0,0 +1,72 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var coins = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var sum = int.Parse(Console.ReadLine());
+
+            var selectedCoins = ChooseCoins(coins, sum);
+
+
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+            foreach (var coin in selectedCoins)
+            {
+                Console.WriteLine($"{coin.Value} coin(s) with value {coin.Key}");
+            }
+
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+
+            var sortedCoins = coins.OrderByDescending(x => x).ToArray();
+
+            var chosenCoins = new Dictionary<int, int>();
+
+            var currentSum = 0;
+            var coinIndex = 0;
+
+            while (currentSum != targetSum && coinIndex < sortedCoins.Length)
+            {
+
+                var currentCoinValue = sortedCoins[coinIndex];
+
+
+                var diff = targetSum - currentSum;
+
+                var numberOfCoins = diff / currentCoinValue;
+
+                if (currentSum + currentCoinValue <= targetSum)
+                {
+                    chosenCoins.Add(currentCoinValue, numberOfCoins);
+
+
+                    currentSum += numberOfCoins * currentCoinValue;
+
+
+                }
+
+                coinIndex++;
+
+
+            }
+
+            if (currentSum != targetSum)
+            {
+                throw new InvalidOperationException();
+            }
+            return chosenCoins;
+        }
+    }
+}
