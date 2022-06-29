767f3d39874545b957b71793cc753e9f7d4d8283
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-30 00:49:28 +0100

    last project - P07_Binary_Search in this solution added

---
+++ C# Advanced - May 2022/S17_Basic_Algorithms_Exercise/P07_Binary_Search/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var key = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine(BinarySearch.IndexOf(arr, key));
+            
+        }
+
+
+        public class BinarySearch
+        {
+            public static int IndexOf(int[] arr, int key)
+            {
+                var lo = 0;
+                var hi = arr.Length - 1;
+
+                while (lo <= hi)
+                {
+
+                    var mid = lo + (hi - lo) / 2;
+
+                    if (key < arr[mid])
+                    {
+                        hi = mid - 1;
+                    }
+                    else if (key > arr[mid])
+                    {
+                        lo = mid + 1;
+                    }
+                    else
+                    {
+                        return mid;
+                    }
+                }
+
+                return -1;
+            }
+        }
+    }
+}
