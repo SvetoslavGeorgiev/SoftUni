8a5c7408646e31d8fe1577a8c5674be5080eaf18
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-29 21:35:42 +0100

    new project - P04_Set_Cover added

---
+++ C# Advanced - May 2022/S17_Basic_Algorithms_Exercise/P04_Set_Cover/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var numberOfsets = int.Parse(Console.ReadLine());
+            var sestsOfNumbers = new int[numberOfsets][];
+
+            for (int row = 0; row < sestsOfNumbers.Length; row++)
+            {
+                var rowValue = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sestsOfNumbers[row] = new int[rowValue.Length];
+
+                for (int col = 0; col < sestsOfNumbers[row].Length; col++)
+                {
+                    sestsOfNumbers[row][col] = rowValue[col];
+                }
+            }
+
+            List<int[]> selectedSets = ChooseSets(sestsOfNumbers.ToList(), universe.ToList());
+
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+            }
+
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+
+            var selectedSets = new List<int[]>();
+
+            while (universe.Any())
+            {
+
+                var longestSet = sets.OrderByDescending(s => s.Count(x => universe.Contains(x))).FirstOrDefault();
+
+                selectedSets.Add(longestSet);
+                sets.Remove(longestSet);
+
+                foreach (var item in longestSet)
+                {
+                    universe.Remove(item);
+                }
+            }
+
+            return selectedSets;
+        }
+    }
+}
