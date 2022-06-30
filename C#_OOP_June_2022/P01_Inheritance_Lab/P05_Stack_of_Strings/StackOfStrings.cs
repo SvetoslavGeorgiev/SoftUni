68d2554b1432ebe0e4a9f0ddaa819397687d77e3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-06-30 02:59:47 +0100

    added last project in the solution

---
+++ C#_OOP_June_2022/P01_Inheritance_Lab/P05_Stack_of_Strings/StackOfStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+
+
+        public bool IsEmpty()
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+
+            foreach (string str in collection)
+            {
+                Push(str);
+            }
+
+            return this;
+        }
+
+    }
+}
