076977d707c0a9229a7770e65f1221085fc5d18b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-02 13:25:52 +0100

    new project - P04_Team added

---
+++ C#_OOP_June_2022/S03_Encapsulation_Lab/P04_Team/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                    persons.Add(person);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+            }
+            //var parcentage = decimal.Parse(Console.ReadLine());
+
+            Team team = new Team("SoftUni");
+            persons.ForEach(p => team.AddPlayer(p));
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"First team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
