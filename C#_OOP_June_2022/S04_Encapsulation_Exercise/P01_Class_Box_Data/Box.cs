ce9bfde081d0e5e56d1c77fc1ac246e74703b12b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-02 16:50:52 +0100

    new solution and first project in it

---
+++ C#_OOP_June_2022/S04_Encapsulation_Exercise/P01_Class_Box_Data/Box.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                //if (value <= 0)
+                //{
+                //    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                //}
+                ValidateForPozitiveNumber(value, "Length");
+                length = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                //if (value <= 0)
+                //{
+                //    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                //}
+                ValidateForPozitiveNumber(value, "Width");
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                //if (value <= 0)
+                //{
+                //    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                //}
+                ValidateForPozitiveNumber(value, "Height");
+                height = value;
+            }
+        }
+
+        //area formulas:
+        //Volume = lwh
+        //Lateral Surface Area = 2lh + 2wh
+        //Surface Area = 2lw + 2lh + 2wh
+
+        public double SurfaceArea()
+        {
+            return 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        private void ValidateForPozitiveNumber(double number, string property)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException($"{property} cannot be zero or negative.");
+            }
+        }
+
+    }
+}
---
+++ C#_OOP_June_2022/S04_Encapsulation_Exercise/P01_Class_Box_Data/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            (var length, var width, var height) = (double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            Box box;
+
+            try
+            {
+                box = new Box(length, width, height);
+
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+    }
+}
