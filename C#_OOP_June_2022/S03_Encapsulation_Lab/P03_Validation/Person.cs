9997dee0dd30154e5a8bc272782a8d1b6dd821ed
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-02 13:08:58 +0100

    new project - P03_Validation added

---
+++ C#_OOP_June_2022/S03_Encapsulation_Lab/P03_Validation/Person.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+
+        public string FirstName 
+        { 
+            get => firstName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException($"{nameof(firstName)} First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName 
+        { 
+            get => lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException($"{nameof(lastName)} Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            } 
+        }
+
+        public int Age 
+        { 
+            get => age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(age)} Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+
+
+        public decimal Salary 
+        { 
+            get => salary;
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException($"{nameof(salary)} Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+
+    }
+}
---
+++ C#_OOP_June_2022/S03_Encapsulation_Lab/P03_Validation/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            //5
+            //Andrew Williams -6 2200
+            //B Gomez 57 3333
+            //Carolina Richards 27 670
+            //Gilbert H 44 666.66
+            //Joshua Anderson 35 300
+            //20
+
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                    persons.Add(person);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+            }
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
