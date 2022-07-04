f85e13508a0bd74a807044d46ebc0e7cca5ba1ff
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-04 18:59:36 +0100

    new VS solution added and first project - P01_Define_an_Interface_IPerson in it

---
+++ C#_OOP_June_2022/S06_Interfaces_and_Abstraction_Exercise/P01_Define_an_Interface_IPerson/Citizen.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
---
+++ C#_OOP_June_2022/S06_Interfaces_and_Abstraction_Exercise/P01_Define_an_Interface_IPerson/IPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonInfo
+{
+    public interface IPerson
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
