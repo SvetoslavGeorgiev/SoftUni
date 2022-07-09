eff65ef1bc1bd4c8a6af42b66edd7a29b1522446
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-09 12:26:54 +0100

    new solution started and first project added in it

---
+++ C#_OOP_June_2022/S07_Polymorphism_Lab/P01_Math_Operations/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class MathOperations
+    {
+
+
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Add(double number1, double number2, double number3)
+        {
+            return number1 + number2 + number3;
+        }
+
+        public decimal Add(decimal number1, decimal number2, decimal number3)
+        {
+            return number1 + number2 + number3;
+        }
+    }
+}
