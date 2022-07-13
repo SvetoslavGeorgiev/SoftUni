cef887628769f82b93f1cbdc917949afef690e03
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-13 12:24:19 +0100

    project - CarManager.Tests completed

--- C#_OOP_June_2022/S13_Unit_Testing_Exercise/FightingArena.Tests/ArenaTests.cs
+++ C#_OOP_June_2022/S13_Unit_Testing_Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,98 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class ArenaTests
     {
+
+        [Test]
+        public void ConstructorCheck()
+        {
+            var arena = new Arena(); 
+
+            Assert.IsNotNull(arena);
+
+            Assert.AreEqual(0, arena.Warriors.Count);
+        }
+
+        [Test]
+        public void AddingWorriorInToTheArena()
+        {
+            var arena = new Arena();
+            var worrior = new Warrior("Pesho", 55, 150);
+
+            arena.Enroll(worrior);
+            Assert.AreEqual(1, arena.Count);
+        }
+
+        [Test]
+        public void WhenAddingWorriorInToTheArenaWithExistingNameShouldThrowInvalidOperationExeption()
+        {
+            var arena = new Arena();
+            var worrior = new Warrior("Pesho", 55, 150);
+
+            arena.Enroll(worrior);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(new Warrior("Pesho", 150, 350)));
+        }
+
+        [Test]
+        public void FigthShouldThrowInvalidOperationExceptionIfAttackerNotInTheArena()
+        {
+            var arena = new Arena();
+            var worrior = new Warrior("Gosho", 55, 150);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+            var worrior2 = new Warrior("Ivan", 55, 65);
+
+
+            arena.Enroll(worrior);
+            arena.Enroll(worrior1);
+            arena.Enroll(worrior2);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Stoyan", "Pesho"));
+        }
+
+        [Test]
+        public void FigthShouldThrowInvalidOperationExceptionIfDefenderNotInTheArena()
+        {
+            var arena = new Arena();
+            var worrior = new Warrior("Gosho", 55, 150);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+            var worrior2 = new Warrior("Ivan", 55, 65);
+
+
+            arena.Enroll(worrior);
+            arena.Enroll(worrior1);
+            arena.Enroll(worrior2);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Pesho", "Stoyan"));
+        }
+
+        [Test]
+        public void FigthShouldChancheTheHpIfAttackerAndDefenderExist()
+        {
+            var arena = new Arena();
+            var worrior = new Warrior("Gosho", 55, 150);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+            var worrior2 = new Warrior("Ivan", 55, 65);
+
+
+            arena.Enroll(worrior);
+            arena.Enroll(worrior1);
+            arena.Enroll(worrior2);
+
+            arena.Fight("Gosho", "Pesho");
+
+            Assert.AreEqual(95, worrior.HP);
+            Assert.AreEqual(0, worrior1.HP);
+
+            arena.Fight("Gosho", "Ivan");
+
+            Assert.AreEqual(10, worrior2.HP);
+        }
+
 
     }
 }
--- C#_OOP_June_2022/S13_Unit_Testing_Exercise/FightingArena.Tests/WarriorTests.cs
+++ C#_OOP_June_2022/S13_Unit_Testing_Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,144 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class WarriorTests
     {
 
+        [Test]
+        public void ConstruvtorCheckingForCreatingWarrior()
+        {
+            var worrior = new Warrior("Pesho", 55, 150);
+
+            Assert.That(worrior, Is.Not.Null);
+        }
+
+
+        //[Test]
+        //public void ConstruvtorCheckingForCreatingWarriorAndItsProperties()
+        //{
+
+        //    var worrior = new Warrior("Pesho", 55, 150);
+        //    var worrior1 = new Warrior("Pesho", 55, 150);
+
+        //    Assert.AreEqual(worrior, worrior1);
+        //    Assert.AreEqual(worrior, new Warrior("Pesho", 55, 150));
+
+        //}
+
+        //[Test]
+        //public void ConstruvtorCheckingForCreatingWarriorAndItProperties()
+        //{
+        //    var warriors = new Warrior[5];
+
+        //    for (int i = 0; i < warriors.Length; i++)
+        //    {
+        //        warriors[i] = new Warrior($"Pesho{i}", 55 + i, 150 + i);
+        //    }
+        //    var worrior = warriors[3];
+
+        //    Assert.AreEqual(new Warrior("Pesho4", 59, 154), worrior);
+        //}
+
+
+        [Test]
+        public void ConstruvtorCheckingForCreatingWarriorAndItProperties()
+        {
+            var worrior = new Warrior("Pesho", 55, 150);
+            var worrior1 = new Warrior("Pesho", 55, 150);
+
+            Assert.AreEqual(worrior.Name, worrior1.Name);
+            Assert.AreEqual(worrior.HP, worrior1.HP);
+            Assert.AreEqual(worrior.Damage, worrior1.Damage);
+        }
+
+        [Test]
+        public void CreatingWarriorWithNullEmptyOrWhiteSpacesNameShouldThrowArgumentException()
+        {
+
+            Assert.Throws<ArgumentException>(() => new Warrior(null, 55, 150));
+            Assert.Throws<ArgumentException>(() => new Warrior(" ", 55, 150));
+            Assert.Throws<ArgumentException>(() => new Warrior(string.Empty, 55, 150));
+        }
+
+
+        [Test]
+        public void CreatingWarriorWitheroOrNegativeDamageShouldThrowArgumentException()
+        {
+
+            Assert.Throws<ArgumentException>(() => new Warrior("Pesho", 0, 150));
+            Assert.Throws<ArgumentException>(() => new Warrior("Pesho", -25, 150));
+        }
+
+
+        [Test]
+        public void CreatingWarriorNegativeHpShouldThrowArgumentException()
+        {
+
+            Assert.Throws<ArgumentException>(() => new Warrior("Pesho", 15, -25));
+        }
+
+        [Test]
+        public void MinHpShouldBeMoreThen30ToAttackOtherWorriors()
+        {
+
+            var worrior = new Warrior("Pesho", 55, 30);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+
+            Assert.Throws<InvalidOperationException>(() => worrior.Attack(worrior1));
+
+            worrior = new Warrior("Pesho", 55, 5);
+            worrior1 = new Warrior("Pesho", 55, 35);
+
+            Assert.Throws<InvalidOperationException>(() => worrior.Attack(worrior1));
+        }
+
+
+        [Test]
+        public void EnemyHpMustBeGreaterThan_MIN_ATTACK_HP_InOrderToAttackHim()
+        {
+
+            var worrior = new Warrior("Pesho", 55, 55);
+            var worrior1 = new Warrior("Pesho", 55, 29);
+
+            Assert.Throws<InvalidOperationException>(() => worrior.Attack(worrior1));
+
+            worrior = new Warrior("Pesho", 55, 59);
+            worrior1 = new Warrior("Pesho", 55, 5);
+
+            Assert.Throws<InvalidOperationException>(() => worrior.Attack(worrior1));
+        }
+
+        [Test]
+        public void WhenAttackTooStrongEnemyShoudThrowInvalidOperationException()
+        {
+
+            var worrior = new Warrior("Pesho", 55, 50);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+
+            Assert.Throws<InvalidOperationException>(() => worrior.Attack(worrior1));
+        }
+
+
+        [Test]
+        public void WhenAttackHpShouldChanche()
+        {
+
+            var worrior = new Warrior("Pesho", 55, 150);
+            var worrior1 = new Warrior("Pesho", 55, 35);
+            var worrior2 = new Warrior("Pesho", 55, 65);
+            worrior.Attack(worrior1);
+            Assert.AreEqual(95, worrior.HP);
+            Assert.AreEqual(0, worrior1.HP);
+
+            worrior.Attack(worrior2);
+
+
+            Assert.AreEqual(10, worrior2.HP);
+
+
+        }
     }
 }
