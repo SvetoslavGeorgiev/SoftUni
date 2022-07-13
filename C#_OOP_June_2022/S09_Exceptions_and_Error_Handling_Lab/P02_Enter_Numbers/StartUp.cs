a125a0fea353e9fa39157fcfa37241797efee418
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-13 14:28:19 +0100

    new project P02_Enter_Numbers added

---
+++ C#_OOP_June_2022/S09_Exceptions_and_Error_Handling_Lab/P02_Enter_Numbers/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace P02_Enter_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            var numArray = new List<int>();
+            var startNum = 1;
+            var endNum = 100;
+
+            while (numArray.Count < 10)
+            {
+                try
+                {
+                    startNum = ReadNumber(startNum, endNum);
+                    numArray.Add(startNum);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+                finally
+                {
+                    if (numArray.Count == 10)
+                    {
+                        Console.WriteLine($"{string.Join(", ", numArray)}");
+                    }
+                }
+            }
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int resultNum))
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+
+            var num = resultNum;
+
+            if (num <= start || num >= end)
+            {
+                var temp = start;
+
+                throw new ArgumentException($"Your number is not in range {temp} - 100!");
+
+            }
+
+            return num;
+
+        }
+    }
+}
