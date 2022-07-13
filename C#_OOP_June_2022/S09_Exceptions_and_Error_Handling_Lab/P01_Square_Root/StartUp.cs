ebc616053b367b07d3f2fe748ac9fc344c078830
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-13 12:53:19 +0100

    new solution Started and First project in it

---
+++ C#_OOP_June_2022/S09_Exceptions_and_Error_Handling_Lab/P01_Square_Root/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace P01_Square_Root
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            var number = int.Parse(Console.ReadLine());
+
+            
+
+            try
+            {
+                Console.WriteLine(TryGetSquareRoot(number));
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+
+        }
+
+        private static int TryGetSquareRoot(int number)
+        {
+
+
+
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+            return (int)Math.Sqrt(number);
+        }
+    }
+}
