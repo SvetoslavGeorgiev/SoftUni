e5e182ee271c01bebbc9c81566875cb7855b8185
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-17 22:10:53 +0100

    new solution and first project - P01_Stealer added in it

---
+++ C#_OOP_June_2022/S10_Reflection_and_Attributes_Lab/P01_Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            var sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
