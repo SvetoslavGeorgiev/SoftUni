c695dd1a0910abdf609d4819dcf1011871d754a7
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-18 21:04:41 +0100

    project - P06_Code_Tracker added

---
+++ C#_OOP_June_2022/S10_Reflection_and_Attributes_Lab/P06_Code_Tracker/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+
+}
---
+++ C#_OOP_June_2022/S10_Reflection_and_Attributes_Lab/P06_Code_Tracker/Tracker.cs
@@ -0,0 +1,38 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public Tracker()
+        {
+
+
+
+
+        }
+
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
