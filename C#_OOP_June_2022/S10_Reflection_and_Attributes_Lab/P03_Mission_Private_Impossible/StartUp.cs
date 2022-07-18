b62a6b8f16eb9beffc7f2fec2816c7eb23ba9e2e
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-18 20:13:32 +0100

    project - P03_Mission_Private_Impossible added

---
+++ C#_OOP_June_2022/S10_Reflection_and_Attributes_Lab/P03_Mission_Private_Impossible/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
