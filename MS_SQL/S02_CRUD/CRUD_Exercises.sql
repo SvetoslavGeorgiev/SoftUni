f255a8d2ef5e466eb3a34d7630bcf7f6dfb2d5b0
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-07-31 16:24:10 +0100

    first 5 SQL queries done for - S02_CRUD in MS_SQL course

---
+++ MS_SQL/S02_CRUD/CRUD_Exercises.sql
@@ -0,0 +1,18 @@
+-- 1.	Examine the Databases
+
+--2.	Find All the Information About Departments
+
+SELECT * FROM [Departments]
+
+-- 3.	Find all Department Names
+
+SELECT [Name] FROM [Departments]
+
+-- 4.	Find Salary of Each Employee
+
+SELECT [FirstName], [LastName], [Salary] FROM [Employees]
+
+--5.	Find Full Name of Each Employee
+
+
+SELECT [FirstName], [MiddleName], [LastName] FROM [Employees]
