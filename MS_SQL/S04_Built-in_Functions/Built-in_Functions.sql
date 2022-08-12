9abbe36c1ad7f3ed362069fca181c4350db90e0f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-12 23:15:56 +0100

    new queries adeed to S04_Built-in_Functions

--- MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
+++ MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
@@ -72,8 +72,12 @@ SELECT [FirstName], [LastName]
 
 --Problem 10.  Rank Employees by Salary
 
-
-
+  SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+  	   DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
+  	AS [Rank]
+    FROM [Employees]
+   WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC
 
 --Problem 11.	Find All Employees with Rank 2 *
 SELECT *
@@ -89,4 +93,3 @@ WHERE Rank = 2
 ORDER BY [Salary] DESC
 
 
-git commit -m "new queries adeed to S04_Built-in_Functions"
