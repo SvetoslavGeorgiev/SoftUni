403a765e1c3250005a7074ef1b4be0ca78e11c88
Svetoslav Georgiev <54979873+SvetoslavGeorgiev@users.noreply.github.com>
2022-08-18 22:51:17 +0100

    Added new query for problem 13

--- MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
+++ MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
@@ -98,3 +98,12 @@ SELECT CountryName AS [Country Name], IsoCode AS [Iso Code]
   FROM Countries
  WHERE LEN(CountryName) - LEN(REPLACE(CountryName,'a','')) >=3
  ORDER BY [Iso Code]
+
+--Problem 13. Mix of Peak and River Names
+
+SELECT Peaks.PeakName, Rivers.RiverName, 
+LOWER((Peaks.PeakName) + SUBSTRING(Rivers.RiverName,2,LEN(Rivers.Rivername))) AS 'Mix' 
+FROM Peaks
+JOIN Rivers
+ON RIGHT(Peaks.PeakName,1) = LEFT(Rivers.RiverName,1)
+ORDER BY Mix
