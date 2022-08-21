283a99ba32e1fdc3ca62009ddc97112b39679f17
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-21 08:29:55 +0100

    new querie for problem 16 adeed to S04_Built-in_Functions

--- MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
+++ MS_SQL/S04_Built-in_Functions/Built-in_Functions.sql
@@ -87,16 +87,15 @@ ORDER BY [Salary] DESC
 --Problem 11.	Find All Employees with Rank 2 *
 SELECT *
     FROM (
-         SELECT [EmployeeID],
-                [FirstName],
-                [LastName],
-                [Salary],
+         SELECT [EmployeeID], [FirstName], [LastName], [Salary],
         DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID]) 
 		     AS [Rank]
            FROM [Employees]
-          WHERE [Salary] BETWEEN 10000 AND 50000) AS MyTable
-          WHERE [Rank] = 2
-       ORDER BY [Salary] DESC
+          WHERE [Salary] BETWEEN 10000 AND 50000
+		  ) 
+	   AS [MyTable]
+    WHERE [Rank] = 2
+ ORDER BY [Salary] DESC
 
 --Problem 12.	 Countries Holding 'A' 
 
