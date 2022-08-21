7b4b148e1427b2c291d7fb4e62504b9ff11fb17d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-21 11:01:59 +0100

    problem 2 query added to S05_Subqueries_and_Joins

--- MS_SQL/S05_Subqueries_and_Joins/Subqueries_and_Joins.sql
+++ MS_SQL/S05_Subqueries_and_Joins/Subqueries_and_Joins.sql
@@ -6,3 +6,19 @@ SELECT TOP(5) [EmployeeID], [JobTitle], [e].[AddressID], [a].[AddressText]
 	ON [e].[AddressID] = [a].[AddressID]
 	ORDER BY [e].[AddressID]
 
+
+--Problem 2.	Addresses with Towns
+
+SELECT TOP(50) [FirstName], [LastName], [t].[Name] AS [Town], [a].[AddressText]
+	FROM [Employees] AS [e]
+	JOIN [Addresses] AS [a]
+	ON [e].[AddressID] = [a].[AddressID]
+	JOIN [Towns] AS [t]
+	ON [a].[TownID] = [t].TownID
+	ORDER BY [e].[FirstName], [e].[LastName]
+
+--Problem 3.	Sales Employee
+
+
+
+
