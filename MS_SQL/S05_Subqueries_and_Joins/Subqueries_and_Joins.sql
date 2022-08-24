0ee8ac0d0916073eaaff90549d72a3758f4bc938
Svetoslav Georgiev <54979873+SvetoslavGeorgiev@users.noreply.github.com>
2022-08-24 22:44:02 +0100

    Added query for problem 14 in Subqueries_and_Joins

--- MS_SQL/S05_Subqueries_and_Joins/Subqueries_and_Joins.sql
+++ MS_SQL/S05_Subqueries_and_Joins/Subqueries_and_Joins.sql
@@ -150,6 +150,20 @@ LEFT JOIN [MountainsCountries] AS [mc]
     WHERE [c].[CountryCode] IN ('BG', 'US', 'RU')
  GROUP BY [c].[CountryCode]
 
+--Problem 14. Countries With or Without Rivers
+
+SELECT TOP 5
+  c.CountryName,
+  r.RiverName
+FROM Countries AS c
+  INNER JOIN Continents AS con
+    ON c.ContinentCode = con.ContinentCode
+  LEFT JOIN CountriesRivers cr
+    ON c.CountryCode = cr.CountryCode
+  LEFT JOIN Rivers r
+    ON cr.RiverId = r.Id
+WHERE con.ContinentName = 'Africa'
+ORDER BY c.CountryName ASC
 
 SELECT *
    FROM [MountainsCountries]
