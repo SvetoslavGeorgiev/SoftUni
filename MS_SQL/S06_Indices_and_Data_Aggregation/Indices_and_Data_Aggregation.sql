b50cf30fed03f882126da56df0e19ee8c0d7f41b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-30 07:57:27 +0100

    query for Problem 7 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -43,4 +43,11 @@ GROUP BY [DepositGroup]
 
 --Problem 7. Deposits Filter
 
+  SELECT [DepositGroup], SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+   WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+  HAVING SUM([DepositAmount]) < 150000
+ORDER BY [TotalSum] DESC
+
 
