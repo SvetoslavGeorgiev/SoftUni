72ef991ab8b0cfbf0c01b800f0e855298fc146d3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-30 07:20:34 +0100

    query for Problem 2 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -2,4 +2,14 @@ USE [Gringotts]
 
 --Problem 1. Records’ Count
 
+SELECT TOP(1) SUM([Id]) AS [Count]
+	    FROM [WizzardDeposits]
+	GROUP BY [Id]
+	ORDER BY [Id] DESC
 
+--Problem 2. Longest Magic Wand
+
+SELECT TOP(1) [MagicWandSize] AS [LongestMagicWand]
+	FROM [WizzardDeposits]
+	GROUP BY [MagicWandSize]
+	ORDER BY [LongestMagicWand] DESC 
