e732e755fe29e194c40189840cd2dda6f2109b86
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-08-31 22:24:45 +0100

    query for Problem 9 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -56,3 +56,23 @@ ORDER BY [TotalSum] DESC
     FROM [WizzardDeposits]
 GROUP BY [DepositGroup], [MagicWandCreator]
 ORDER BY [MagicWandCreator], [DepositGroup]
+
+--Problem 9. Age Groups
+SELECT [AgeGroup], COUNT(*) AS [WizardCount]
+  FROM (
+		SELECT [Age],
+			   CASE 
+					WHEN [Age] BETWEEN 0 AND 10 THEN '[0-10]'
+					WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+					WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+					WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+					WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+					WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+					WHEN [Age] >= 61 THEN '[61+]'
+				END
+				AS [AgeGroup]
+			FROM [WizzardDeposits]
+        ) AS [AgeGroups]
+GROUP BY [AgeGroup]
+
+
