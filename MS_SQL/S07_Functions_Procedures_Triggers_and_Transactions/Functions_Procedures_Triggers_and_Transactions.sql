067faf880c7cd80de8aec350fd8c2b7bc708c5a8
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-03 23:50:24 +0100

    query for Problem 1  in -> S07_Functions_Procedures_Triggers_and_Transactions

--- MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
+++ MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
@@ -1,2 +1,15 @@
 --Problem 1.	Employees with Salary Above 35000
 
+GO
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
+AS
+BEGIN
+	SELECT [FirstName],
+		   [LastName]
+	  FROM [Employees]
+	 WHERE [Salary] > 35000
+END
+
+
+GO
