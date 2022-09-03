405ebb0e11a1246654d17c5cd433308de7bee20f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-03 21:16:06 +0100

    query for Problem 12 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -93,6 +93,20 @@ ORDER BY [FirstLetter]
 GROUP BY [DepositGroup], [IsDepositExpired]
 ORDER BY [DepositGroup] DESC, [IsDepositExpired]
 
+--Problem 12. * Rich Wizard, Poor Wizard
 
-SELECT *
-    FROM [WizzardDeposits]
+SELECT SUM([Host Wizard Deposit] - [Guest Wizard Deposit])
+ FROM 
+     (
+		SELECT [FirstName] 
+			AS [Host Wizard], 
+			   [DepositAmount] 
+			AS [Host Wizard Deposit],
+			   LEAD([FirstName]) OVER(ORDER BY [Id]) 
+			AS [Guest Wizard],
+			   LEAD([DepositAmount]) OVER(ORDER BY [Id]) 
+			AS [Guest Wizard Deposit]
+		  FROM [WizzardDeposits]
+	 ) AS [HostGuestQuery]
+  WHERE [Guest Wizard] IS NOT NULL
+
