6d56208c1a466058c8a5c534cae9e92ab8cc9ce4
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-04 00:00:54 +0100

    query for Problem 2  in -> S07_Functions_Procedures_Triggers_and_Transactions

--- MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
+++ MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
@@ -2,13 +2,30 @@
 
 GO
 
-CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
+CREATE PROCEDURE [usp_GetEmployeesSalaryAbove35000]
 AS
 BEGIN
 	SELECT [FirstName],
 		   [LastName]
 	  FROM [Employees]
 	 WHERE [Salary] > 35000
+END
+
+
+GO
+
+--Problem 2.	Employees with Salary Above Number
+
+
+GO
+
+CREATE OR ALTER PROCEDURE [usp_GetEmployeesSalaryAboveNumber] @minSalary DECIMAL(18, 4)
+AS
+BEGIN
+	SELECT [FirstName],
+		   [LastName]
+	  FROM [Employees]
+	 WHERE [Salary] >= @minSalary
 END
 
 
