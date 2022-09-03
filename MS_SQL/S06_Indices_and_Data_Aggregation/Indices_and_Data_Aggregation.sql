fca4bd5a66e0cc28f15566fbdec39bf785d0b3c9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-03 22:10:17 +0100

    query for Problem 15 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -135,7 +135,21 @@ GROUP BY [DepartmentID]
 
 --Problem 15. Employees Average Salaries
 
-
+SELECT * INTO [EmployeesAS] 
+         FROM [Employees]
+        WHERE [Salary] >= 30000
+ 
+DELETE FROM [EmployeesAS]
+      WHERE [ManagerID] = 42
+ 
+UPDATE [EmployeesAS]
+   SET [Salary] += 5000
+ WHERE [DepartmentID] = 1
+ 
+  SELECT [DepartmentID],
+     AVG([Salary]) AS [AverageSalary]
+    FROM [EmployeesAS]
+GROUP BY [DepartmentID]
 
 
 
