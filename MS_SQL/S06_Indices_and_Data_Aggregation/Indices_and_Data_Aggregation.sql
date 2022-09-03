7798dccea3e3b0a7ce379faa2f69c7e74dd1ca89
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-03 22:13:07 +0100

    query for Problem 17 in -> S06_Indices_and_Data_Aggregation

--- MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
+++ MS_SQL/S06_Indices_and_Data_Aggregation/Indices_and_Data_Aggregation.sql
@@ -160,3 +160,11 @@ SELECT [DepartmentID], MAX([Salary]) AS [MaxSalary]
     FROM [Employees]
 GROUP BY [DepartmentID]
 HAVING NOT MAX([Salary]) BETWEEN 30000 AND 70000
+
+
+--17. Employees Count Salaries
+
+SELECT COUNT(*) AS [Count]
+	FROM [Employees]
+	WHERE [ManagerID] IS NULL
+
