54a95b6206ba73b733eb73a2454acf9cd5bba950
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-04 21:31:12 +0100

    new project P03_Employees_Full_Information added

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P03_Employees_Full_Information/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using Models;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(dbContext);
+
+            System.Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            Employee[] allEmployees = context.Employees.OrderBy(e => e.EmployeeId).ToArray();
+
+            foreach (var e in allEmployees)
+            {
+                output
+                    .AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
