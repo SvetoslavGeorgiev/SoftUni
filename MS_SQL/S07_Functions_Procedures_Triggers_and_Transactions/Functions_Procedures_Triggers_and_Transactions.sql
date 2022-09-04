67987c5c1995e97c1dcb8e78434b5904f98b8ac1
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-04 11:49:13 +0100

    Update query for Problem 12

--- MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
+++ MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
@@ -259,11 +259,20 @@ CREATE OR ALTER PROCEDURE usp_CalculateFutureValueForAccount (@accountId INT, @y
 AS
 BEGIN
 
-	SELECT [a].[Id] AS [Account Id], [FirstName] AS [First Name], [LastName] AS [Last Name], [Balance] AS [Current Balance], dbo.ufn_CalculateFutureValue([Balance], @yearlyInterestRate, 5) AS [Balance in 5 years]
-		FROM [AccountHolders] AS [ah]
-   LEFT JOIN [Accounts] AS [a]
-		  ON [ah].[Id] = [a].[AccountHolderId]
-	   WHERE [a].[Id] = @accountId
+   SELECT [a].[Id] 
+	   AS [Account Id], 
+		  [FirstName] 
+	   AS [First Name], 
+		  [LastName] 
+	   AS [Last Name], 
+		  [Balance] 
+	   AS [Current Balance], 
+		  [dbo].[ufn_CalculateFutureValue]([Balance], @yearlyInterestRate, 5) 
+	   AS [Balance in 5 years]
+	 FROM [AccountHolders] AS [ah]
+LEFT JOIN [Accounts] AS [a]
+	   ON [ah].[Id] = [a].[AccountHolderId]
+	WHERE [a].[Id] = @accountId
 	
 END
 
