ee79612ff535ac3929187e5d89dfdb087ee57ebe
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-04 15:09:18 +0100

    query for Problem 14 in -> S07_Functions_Procedures_Triggers_and_Transactions

--- MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
+++ MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
@@ -307,3 +307,39 @@ GO
 
 SELECT *
  FROM [dbo].[ufn_CashInUsersGames]('Love in a mist')
+
+
+
+--         Exercises: Triggers and Transactions
+
+
+--Problem 1(14) Create Table Logs
+
+USE [Bank]
+
+CREATE TABLE [Logs]
+(
+	[LogId] INT PRIMARY KEY IDENTITY,
+	[AccountId] INT NOT NULL FOREIGN KEY REFERENCES [Accounts]([Id]),
+	[OldSum] MONEY NOT NULL,
+	[NewSum] MONEY NOT NULL
+
+)
+
+DROP TABLE [Logs]
+
+GO
+
+CREATE TRIGGER tr_OnLogsAddLogRecord
+ON [Accounts] FOR UPDATE
+AS
+BEGIN
+	INSERT INTO [Logs] ([AccountId], [OldSum], [NewSum])
+	SELECT [i].[Id], [d].[Balance], [i].[Balance]
+	  FROM [inserted] AS [i]
+	  JOIN [deleted] AS [d]
+	    ON [i].[Id] = [d].[Id]
+
+END
+
+GO
