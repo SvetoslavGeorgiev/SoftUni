9489ef6409f0dc0049ec0d5d06bbbefd9c268e7d
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-04 01:10:50 +0100

    query for Problem 7  in -> S07_Functions_Procedures_Triggers_and_Transactions

--- MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
+++ MS_SQL/S07_Functions_Procedures_Triggers_and_Transactions/Functions_Procedures_Triggers_and_Transactions.sql
@@ -116,3 +116,32 @@ END
 GO
 
 EXEC [dbo].[usp_EmployeesBySalaryLevel] 'High'
+
+
+--Problem 7.	Define Function
+GO
+
+CREATE OR ALTER FUNCTION [ufn_IsWordComprised](@setOfLetters VARCHAR(50), @word VARCHAR(50)) 
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @currentIndex int = 1;
+
+    WHILE @currentIndex <= LEN(@word)
+	BEGIN
+
+	DECLARE @currentLetter varchar(1) = SUBSTRING(@word, @currentIndex, 1);
+
+	IF(CHARINDEX(@currentLetter, @setOfLetters)) = 0
+	BEGIN
+	RETURN 0;
+	END
+
+	SET @currentIndex += 1;
+	END
+
+    RETURN 1;
+END
+
+
+GO
