9586ec16e709ea82180e482ba83e3c14a4eba550
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-05 06:08:06 +0100

    new project P04_Employees_with_Salary_Over_50_000 added

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P04_Employees_with_Salary_Over_50_000/Models/Town.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftUni.Models
+{
+    using System.Collections.Generic;
+    public class Town
+    {
+        public Town()
+        {
+            Addresses = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P04_Employees_with_Salary_Over_50_000/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(dbContext);
+
+            System.Console.WriteLine(result);
+
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var allEmployees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in allEmployees)
+            {
+                output
+                    .AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
