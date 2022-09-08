1f7bb7cf375ed097eddaca2fb1e93c01193f9f50
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-08 08:24:49 +0100

    added project P05_Employees_from_Research_and_Development

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P05_Employees_from_Research_and_Development/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesFromResearchAndDevelopment(dbContext);
+
+            Console.WriteLine(result);
+
+        }
+
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var allEmployees = context
+                .Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in allEmployees)
+            {
+                output
+                    .AppendLine($"{e.FirstName} {e.LastName} from {e.Name} - ${e.Salary:F2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
