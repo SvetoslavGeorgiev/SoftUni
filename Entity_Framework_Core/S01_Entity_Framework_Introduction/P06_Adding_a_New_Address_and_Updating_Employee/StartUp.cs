2b7bc5fc5858e34ee629e8f54114150efe4232b9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-10 21:17:30 +0100

    added project P06_Adding_a_New_Address_and_Updating_Employee

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P06_Adding_a_New_Address_and_Updating_Employee/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = AddNewAddressToEmployee(dbContext);
+
+            Console.WriteLine(result);
+
+        }
+
+
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            context.Add(newAddress);
+
+            var nakov = context
+                .Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            nakov.Address = newAddress;
+
+            context.SaveChanges();
+
+            var addressText = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText)
+                .ToArray();
+
+
+            foreach (var item in addressText)
+            {
+                output.AppendLine(item);
+            }
+
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
