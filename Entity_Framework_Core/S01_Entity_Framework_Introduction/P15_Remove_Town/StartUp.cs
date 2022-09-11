e3b2056f7dc274b4e52f2ddf743a3602437a7df2
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-11 21:53:12 +0100

    added project P15_Remove_Town

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P15_Remove_Town/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = RemoveTown(dbContext);
+
+            Console.WriteLine(result);
+
+
+        }
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var townToDelete = context
+                .Towns
+                .Where(t => t.Name == "Seattle")
+                .FirstOrDefault();
+
+            var refferedAddresses = context
+                .Addresses
+                .Where(a => a.TownId == townToDelete.TownId)
+                .ToList();
+
+            foreach (var e in context.Employees)
+            {
+                if (refferedAddresses.Any(x => x.AddressId == e.AddressId))
+                {
+                    e.AddressId = null;
+                }
+            }
+
+            var numberOfAddressesDeleted = refferedAddresses.Count;
+            
+
+            context.Addresses.RemoveRange(refferedAddresses);
+            context.Towns.Remove(townToDelete);
+
+            context.SaveChanges();
+
+            return $"{numberOfAddressesDeleted} addresses in Seattle were deleted";
+        }
+    }
+}
