9a83f279b91307032b4e1c0fcb026c41c5ad9b26
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-11 21:02:20 +0100

    added project P13_Find_Employees_by_First_Name_Starting_With_Sa

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P13_Find_Employees_by_First_Name_Starting_With_Sa/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesByFirstNameStartingWithSa(dbContext);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var EmployeesStartWithSa = context
+                .Employees
+                .Where(e => e.FirstName.StartsWith("Sa"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary,
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var employee in EmployeesStartWithSa)
+            {
+
+                output.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle} - (${employee.Salary:F2})");
+
+
+            }
+
+
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
