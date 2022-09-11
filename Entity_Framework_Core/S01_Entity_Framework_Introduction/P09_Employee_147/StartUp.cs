34740e24df474aa883a6e7a29b1bf4c7171994cb
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-11 16:09:51 +0100

    added project P09_Employee_147

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P09_Employee_147/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployee147(dbContext);
+
+            Console.WriteLine(result);
+
+
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var employee147 = context
+                .Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects
+                         .OrderBy(ep => ep.Project.Name)
+                         .Select(ep => new
+                         {
+                             ProjectName = ep.Project.Name,
+                         })
+                         .ToArray()
+                })
+                .First();
+            
+            output.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+            foreach (var project in employee147.Projects)
+            {
+                output.AppendLine($"{project.ProjectName}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
