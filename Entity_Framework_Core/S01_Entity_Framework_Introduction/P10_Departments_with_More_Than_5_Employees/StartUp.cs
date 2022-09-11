4937834bc3d5cde2d161d67bd3bc2a76a8335389
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-11 16:24:17 +0100

    added project P10_Departments_with_More_Than_5_Employees

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P10_Departments_with_More_Than_5_Employees/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetDepartmentsWithMoreThan5Employees(dbContext);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var departmentsWithMoreThen5Employees = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .Select(d => new
+                {
+                    d.Name,
+                    ManagerFirstName = d.Manager.FirstName,
+                    ManagerLastName = d.Manager.LastName,
+                    Employees = d.Employees
+                         .OrderBy(e => e.FirstName)
+                         .ThenBy(e => e.LastName)
+                         .Select(e => new
+                         {
+                             e.FirstName,
+                             e.LastName,
+                             e.JobTitle
+                         })
+                         .ToArray()
+                })
+                .ToArray();
+
+
+            foreach (var departmet in departmentsWithMoreThen5Employees)
+            {
+                output.AppendLine($"{departmet.Name} - {departmet.ManagerFirstName} {departmet.ManagerLastName}");
+                foreach (var employee in departmet.Employees)
+                {
+                    output.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
