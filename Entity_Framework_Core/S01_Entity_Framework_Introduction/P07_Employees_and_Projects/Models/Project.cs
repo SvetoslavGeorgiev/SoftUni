66cf0ff62738413cdc092f29b26717d2428fb9d9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-11 11:37:04 +0100

    added project P07_Employees_and_Projects

---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P07_Employees_and_Projects/Models/Project.cs
@@ -0,0 +1,29 @@
+﻿namespace SoftUni.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public class Project
+    {
+        public Project()
+        {
+            EmployeesProjects = new HashSet<EmployeeProject>();
+        }
+
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public virtual ICollection<EmployeeProject> EmployeesProjects { get; set; }
+
+        public override string ToString()
+        {
+            //--Half-Finger Gloves - 6/1/2002 12:00:00 AM - 6/1/2003 12:00:00 AM
+
+            var endDate = EndDate.HasValue ? EndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished";
+
+            return $"--{Name} - {StartDate.ToString("M/d/yyyy h:mm:ss tt")} - {endDate}";
+        }
+    }
+}
---
+++ Entity_Framework_Core/S01_Entity_Framework_Introduction/P07_Employees_and_Projects/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace SoftUni
+{
+    using System;
+    using Data;
+    using Models;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesInPeriod(dbContext);
+
+            Console.WriteLine(result);
+
+
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var filteredEmployees = context
+                .Employees
+                .Where(e => e.EmployeesProjects
+                              .Any(ep => ep.Project.StartDate.Year >= 2001 && ep.Project.StartDate.Year <= 2003))
+                .Take(10)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    AllProject = e.EmployeesProjects
+                        .Select(ep => new
+                        {
+                            ep.Project.Name,
+                            ep.Project.StartDate,
+                            ep.Project.EndDate
+                        })
+                })
+                .ToList();
+
+            
+
+            foreach (var em in filteredEmployees)
+            {
+                output.AppendLine($"{em.FirstName} {em.LastName} - Manager: {em.ManagerFirstName} {em.ManagerLastName}");
+                foreach (var project in em.AllProject)
+                {
+                    var endDate = project.EndDate.HasValue ? project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished";
+
+                    output.AppendLine($"--{project.Name} - {project.StartDate.ToString("M/d/yyyy h:mm:ss tt")} - {endDate}");
+                }
+            }
+
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
