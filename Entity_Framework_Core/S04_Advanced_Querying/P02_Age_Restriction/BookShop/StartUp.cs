0eab5295f0f215b3ccb0aa19df1f3ee4ba3b85ba
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-18 17:14:57 +0100

    new solution -> S04_Advanced_Querying and first project -> P02_Age_Restriction solition in it

---
+++ Entity_Framework_Core/S04_Advanced_Querying/P02_Age_Restriction/BookShop/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+
+            //DbInitializer.ResetDatabase(db);
+
+            var restriction = Console.ReadLine();
+
+            var result = GetBooksByAgeRestriction(db, restriction);
+
+            Console.WriteLine(result);
+
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+            //AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            AgeRestriction ageRestriction;
+
+            bool isParsed = Enum.TryParse(command, true, out ageRestriction);
+
+            if (!isParsed)
+            {
+                return string.Empty;
+            }
+
+            var books = string.Join($"{Environment.NewLine}", context
+                .Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList());
+
+            return books;
+
+
+
+        }
+    }
+}
