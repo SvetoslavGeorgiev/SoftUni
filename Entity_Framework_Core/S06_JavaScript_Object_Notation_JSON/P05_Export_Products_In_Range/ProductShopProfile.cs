5546fc4a1552928cfc5b5e36d4dcdc507bd201a3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-27 00:32:25 +0100

    solution for problem -> P05_Export_Products_In_Range in P06_JavaScript_Object_Notation_JSON

---
+++ Entity_Framework_Core/S06_JavaScript_Object_Notation_JSON/P05_Export_Products_In_Range/ProductShopProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Category;
+using ProductShop.DTOs.CategoryProduct;
+using ProductShop.DTOs.Product;
+using ProductShop.DTOs.User;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //user
+            this.CreateMap<ImportUserDto, User>();
+
+            //product
+            this.CreateMap<ImportProductDto, Product>();
+            this.CreateMap<Product, ExportProductsInRange>()
+                .ForMember(d => d.SellerFullName, mo => mo.MapFrom(s => $"{s.Seller.FirstName} {s.Seller.LastName}"));
+
+            //category
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProduct
+            this.CreateMap<ImportCategoryProductDto, CategoryProduct>();
+
+
+        }
+    }
+}
