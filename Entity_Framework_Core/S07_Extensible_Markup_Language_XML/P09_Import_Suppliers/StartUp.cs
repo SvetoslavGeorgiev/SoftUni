5503e6839ee2374586354f76b777fe651e946154
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-29 09:10:36 +0100

    solution for problem -> P09_Import_Suppliers in S07_Extensible_Markup_Language_XML

---
+++ Entity_Framework_Core/S07_Extensible_Markup_Language_XML/P09_Import_Suppliers/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace CarDealer
+{
+    using CarDealer.Data;
+    using CarDealer.Dtos.Import;
+    using CarDealer.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            using CarDealerContext dbContext = new CarDealerContext();
+
+            string xml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+            string result = ImportSuppliers(dbContext, xml);
+
+            System.Console.WriteLine(result);
+
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+
+
+
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            ImportSupplierDto[] importSupplierDtos = (ImportSupplierDto[])xmlSerializer.Deserialize(reader);
+
+            Supplier[] suppliers = importSupplierDtos
+                .Select(dto => new Supplier()
+                {
+                    Name = dto.Name,
+                    IsImporter = dto.IsImporter
+                })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
