c913e59aabf59da7b09269ffe000c11e7533efa3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-09-30 09:23:44 +0100

    solution for problem -> P14_Export_Cars_With_Distance in S07_Extensible_Markup_Language_XML

---
+++ Entity_Framework_Core/S07_Extensible_Markup_Language_XML/P14_Export_Cars_With_Distance/Dtos/Export/ExportCarsWithDistanceDto.cs
@@ -0,0 +1,20 @@
+﻿namespace CarDealer.Dtos.Export
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    [XmlType("car")]
+    public class ExportCarsWithDistanceDto
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("travelled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
