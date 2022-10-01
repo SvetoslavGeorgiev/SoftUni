2bf07ae419ea06e3918f60a3209040e6eddfd5a9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-10-01 12:40:39 +0100

    solution for problem -> P16_Export_Local_Suppliers in S07_Extensible_Markup_Language_XML

---
+++ Entity_Framework_Core/S07_Extensible_Markup_Language_XML/P16_Export_Local_Suppliers/Dtos/Import/ImportCarDto.cs
@@ -0,0 +1,26 @@
+﻿namespace CarDealer.Dtos.Import
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [XmlType("Car")]
+    public class ImportCarDto
+    {
+
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("TraveledDistance")]
+        public long TraveledDistance { get; set; }
+
+
+        [XmlArray("parts")]
+        public ImportCarPartsDto[] Parts { get; set; }
+
+
+
+    }
+}
