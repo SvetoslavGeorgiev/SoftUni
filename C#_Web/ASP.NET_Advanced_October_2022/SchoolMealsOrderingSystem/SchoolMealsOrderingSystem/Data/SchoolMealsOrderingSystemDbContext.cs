e6f70826006513ecf4f49543f43509fce0803ab3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-10-30 16:51:55 +0000

    Initial commit

---
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem/Data/SchoolMealsOrderingSystemDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolMealsOrderingSystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class SchoolMealsOrderingSystemDbContext : IdentityDbContext
+    {
+        public SchoolMealsOrderingSystemDbContext(DbContextOptions<SchoolMealsOrderingSystemDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
