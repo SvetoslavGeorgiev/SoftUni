0baf18fd450d438ec0c965dae2f1310ce7ecbedf
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-12-15 12:12:03 +0000

    updating all layouts

---
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Data/Migrations/20221215104955_AddedSchoolUserRelationToMealsTwo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolMealsOrderingSystem.Data.Migrations
+{
+    public partial class AddedSchoolUserRelationToMealsTwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
