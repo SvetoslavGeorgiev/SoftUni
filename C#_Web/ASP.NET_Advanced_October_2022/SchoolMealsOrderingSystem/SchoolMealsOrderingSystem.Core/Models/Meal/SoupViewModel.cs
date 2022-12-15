149a72f2a4bc7e7b06330eb594ab86728e19ee2a
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2022-12-15 21:57:55 +0000

    Added functionality for adding adding soup and view all entries of soup by the current SchoolUser

---
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Core/Models/Meal/SoupViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolMealsOrderingSystem.Core.Models.Meal
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SoupViewModel
+    {
+
+        [UIHint("hidden")]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string Ingredients { get; set; } = null!;
+        public string Allergens { get; set; } = null!;
+
+    }
+}
