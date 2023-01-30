b2530c961b14c980df6db9ceb2899de91befffc5
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-01-30 16:43:49 +0000

    solution for problem 01. Binary Tree

--- Data Structures Fundamentals with C#/S05_Heaps_and_Binary_Trees_Lab/01.BinaryTree/BinaryTree.cs
+++ Data Structures Fundamentals with C#/S05_Heaps_and_Binary_Trees_Lab/01.BinaryTree/BinaryTree.cs
@@ -2,43 +2,120 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
     {
         public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
         {
-            throw new NotImplementedException();
+            Value = element;
+            LeftChild = left;
+            RightChild = right;
         }
 
-        public T Value => throw new NotImplementedException();
+        public T Value { get; private set; }
 
-        public IAbstractBinaryTree<T> LeftChild => throw new NotImplementedException();
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
 
-        public IAbstractBinaryTree<T> RightChild => throw new NotImplementedException();
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
 
         public string AsIndentedPreOrder(int indent)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+
+            PreOrderDfs(sb, indent, this);
+
+            return sb.ToString().Trim();
+
+        }
+
+        private void PreOrderDfs(StringBuilder sb, int indent, IAbstractBinaryTree<T> binaryTree)
+        {
+            sb.Append(new string(' ', indent))
+                .AppendLine(binaryTree.Value.ToString());
+
+
+            if (binaryTree.LeftChild != null)
+            {
+                PreOrderDfs(sb, indent + 2, binaryTree.LeftChild);
+            }
+
+            if (binaryTree.RightChild != null)
+            {
+                PreOrderDfs(sb, indent + 2, binaryTree.RightChild);
+            }
         }
 
         public void ForEachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+            if (LeftChild != null)
+            {
+                LeftChild.ForEachInOrder(action.Invoke);
+            }
+
+            action.Invoke(Value);
+
+            if (RightChild != null)
+            {
+                RightChild.ForEachInOrder(action.Invoke);
+            }
         }
 
         public IEnumerable<IAbstractBinaryTree<T>> InOrder()
         {
-            throw new NotImplementedException();
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.InOrder());
+            }
+
+            result.Add(this);
+
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.InOrder());
+            }
+
+            return result;
         }
 
         public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
         {
-            throw new NotImplementedException();
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.PostOrder());
+            }
+
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.PostOrder());
+            }
+            result.Add(this);
+
+            return result;
         }
 
         public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
         {
-            throw new NotImplementedException();
+            var result = new List<IAbstractBinaryTree<T>>
+            {
+                this
+            };
+
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.PreOrder());
+            }
+
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.PreOrder());
+            }
+            
+            return result;
         }
     }
 }
