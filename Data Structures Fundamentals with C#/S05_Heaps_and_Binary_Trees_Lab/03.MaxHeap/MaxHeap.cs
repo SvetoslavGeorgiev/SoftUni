40ac1f083070dd7900e04b47a45212c3b9acc1c4
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-01-30 19:26:40 +0000

    added solution for problem 03. Max Heap

--- Data Structures Fundamentals with C#/S05_Heaps_and_Binary_Trees_Lab/03.MaxHeap/MaxHeap.cs
+++ Data Structures Fundamentals with C#/S05_Heaps_and_Binary_Trees_Lab/03.MaxHeap/MaxHeap.cs
@@ -5,21 +5,103 @@
 
     public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
     {
-        public int Size => throw new NotImplementedException();
+        private readonly List<T> elements;
+
+        public MaxHeap()
+        {
+            elements = new List<T>();
+        }
+        public int Size => elements.Count;
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            elements.Add(element);
+
+            HeapifyUp(elements.Count - 1);
+        }
+
+        private void HeapifyUp(int index)
+        {
+            var parentIndex = (index - 1) / 2;
+
+            while (index > 0 && elements[index].CompareTo(elements[parentIndex]) > 0)
+            {
+                Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = elements[index];
+            elements[index] = elements[parentIndex];
+            elements[parentIndex] = temp;
         }
 
         public T ExtractMax()
         {
-            throw new NotImplementedException();
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = elements[0];
+            Swap(0, elements.Count - 1);
+            elements.RemoveAt(elements.Count - 1);
+            HeapifyDown(0);
+
+            return element;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            var biggerChildIndex = GetBiggerChildIndex(index);
+
+            while (ValidateIndex(biggerChildIndex) && elements[biggerChildIndex].CompareTo(elements[index]) > 0)
+            {
+                Swap(biggerChildIndex, index);
+                index= biggerChildIndex;
+                biggerChildIndex = GetBiggerChildIndex(index);
+            }
+        }
+
+        private bool ValidateIndex(int index)
+        {
+            return index >= 0 && index < elements.Count;
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex =  index * 2 + 2;
+
+            if (secondChildIndex < elements.Count)
+            {
+                if (elements[firstChildIndex].CompareTo(elements[secondChildIndex]) > 0)
+                {
+                    return firstChildIndex;
+                }
+
+                return secondChildIndex;
+            }
+            else if (firstChildIndex < elements.Count)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return elements[0];
         }
     }
 }
