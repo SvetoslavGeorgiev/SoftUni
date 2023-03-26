b260417309fd3519fde78d48c58681170e837952
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-03-26 14:39:25 +0100

    Added solution for project P01_Recursive_Array_Sum

---
+++ Algorithms Fundamentals/S01_Recursion_and_Backtracking_Lab/P01_Recursive_Array_Sum/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P01_Recursive_Array_Sum
+{
+    using System;
+    using System.Linq;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var index = 0;
+
+            var arr = Console.ReadLine().Split().Select(x => int.Parse(x.ToString())).ToArray();
+
+            Console.WriteLine(GetSum(arr, index));
+        }
+
+        private static int GetSum(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + GetSum(arr, index + 1);
+        }
+    }
+}
