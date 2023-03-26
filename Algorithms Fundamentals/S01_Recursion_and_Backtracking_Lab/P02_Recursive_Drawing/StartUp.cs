d2485425193390bd119fa0171e9974c81b43315b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-03-26 18:33:56 +0100

    Added solution for project P02_Recursive_Drawing

---
+++ Algorithms Fundamentals/S01_Recursion_and_Backtracking_Lab/P02_Recursive_Drawing/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace P02_Recursive_Drawing
+{
+    using System;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+
+            PrintFigure(n);
+
+
+
+        }
+
+        private static void PrintFigure(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            PrintFigure(n - 1);
+
+            Console.WriteLine(new string('#', n));
+
+        }
+    }
+}
