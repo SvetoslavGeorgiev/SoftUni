d1d853db8f49750621f9842242f6cbb8ab080178
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-03-27 16:03:35 +0100

    added solution for project P03_Generating_0_1_Vectors

---
+++ Algorithms Fundamentals/S01_Recursion_and_Backtracking_Lab/P03_Generating_0_1_Vectors/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace P03_Generating_0_1_Vectors
+{
+    using System;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            Gen01(arr, 0);
+
+        }
+
+        private static void Gen01(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                arr[index] = i;
+                Gen01(arr, index + 1);
+            }
+        }
+    }
+}
