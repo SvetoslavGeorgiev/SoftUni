1018a4b13f7ce94bc24df02d9ed15986de9f420f
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-03-27 16:04:29 +0100

    added solution for poblem P04_Recursive_Factorial

---
+++ Algorithms Fundamentals/S01_Recursion_and_Backtracking_Lab/P04_Recursive_Factorial/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace P04_Recursive_Factorial
+{
+    using System;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var number = int.Parse(Console.ReadLine());
+
+            long sum = SumArray(number);
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static long SumArray(int startIndex)
+        {
+
+            if (startIndex == 1)
+            {
+                return startIndex;
+            }
+
+            return startIndex * SumArray(startIndex - 1);
+
+        }
+    }
+}
