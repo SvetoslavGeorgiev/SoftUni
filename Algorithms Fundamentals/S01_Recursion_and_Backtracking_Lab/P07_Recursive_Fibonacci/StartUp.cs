68fa7d9d0ccf72e7a352e8e8600522f27b44f249
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-02 14:42:39 +0100

    added solution for poblem P07_Recursive_Fibonacci

---
+++ Algorithms Fundamentals/S01_Recursion_and_Backtracking_Lab/P07_Recursive_Fibonacci/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace P07_Recursive_Fibonacci
+{
+    using System;
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+
+        private static int GetFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
