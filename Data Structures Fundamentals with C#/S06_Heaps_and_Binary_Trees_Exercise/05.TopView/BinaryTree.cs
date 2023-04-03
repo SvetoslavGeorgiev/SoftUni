75f4d1aeb0d52978491b2b83908f535b4ba32fdd
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-03 23:26:14 +0100

    added solution for problem 05. Top View

--- Data Structures Fundamentals with C#/S06_Heaps_and_Binary_Trees_Exercise/05.TopView/BinaryTree.cs
+++ Data Structures Fundamentals with C#/S06_Heaps_and_Binary_Trees_Exercise/05.TopView/BinaryTree.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
         where T : IComparable<T>
@@ -22,7 +23,29 @@
 
         public List<T> TopView()
         {
-            throw new NotImplementedException();
+            var dict = new Dictionary<int, (T nodeValue, int nodeLevel)>();
+
+            TopView(this, 0, 0, dict);
+
+            return dict.Values.Select(n => n.nodeValue).ToList();
+
+        }
+
+        private void TopView(BinaryTree<T> node, int distance, int level, Dictionary<int, (T nodeValue, int nodeLevel)> dict)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+
+            if (!dict.ContainsKey(distance))
+            {
+                dict.Add(distance, (node.Value, level));
+            }
+
+            TopView(node.LeftChild, distance - 1, level + 1, dict);
+            TopView(node.RightChild, distance + 1, level + 1, dict);
         }
     }
 }
