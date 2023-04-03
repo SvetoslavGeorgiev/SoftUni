102e4ae0ba94dd3b2056355b42a2efe3765b0bee
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-04 00:02:04 +0100

    added solution for problem P01_Permutations_without_Repetition

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P01_Permutations_without_Repetition/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace P01_Permutations_without_Repetition
+{
+    using System;
+    using System.Linq;
+
+    internal class StartUp
+    {
+        private static string[] elements;
+        private static string[] permutations;
+        private static bool[] used;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            permutations = new string[elements.Length];
+
+            used = new bool[elements.Length];
+
+            Permut(0);
+            
+        }
+
+        private static void Permut(int index)
+        {
+            if (index >= permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    permutations[index] = elements[i];
+                    Permut(index + 1);
+                    used[i] = false;
+                }
+            }
+
+        }
+    }
+}
