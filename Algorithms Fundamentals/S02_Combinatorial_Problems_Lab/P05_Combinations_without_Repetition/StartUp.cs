fba7489dbb59338c6f0c799db480d2b2369879f4
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-09 11:08:15 +0100

    added solution for problem 05. Combinations without Repetition

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P05_Combinations_without_Repetition/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace P05_Combinations_without_Repetition
+{
+    using System;
+
+    internal class StartUp
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            GetCombinations(0, 0);
+        }
+
+        private static void GetCombinations(int index, int startIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = startIndex; i < elements.Length; i++)
+            {
+
+                combinations[index] = elements[i];
+                GetCombinations(index + 1, i + 1);
+            }
+        }
+    }
+}
