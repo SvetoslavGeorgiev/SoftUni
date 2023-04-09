b5a650a98500830fa2cbe8d9414d3f7622417d8c
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-09 10:44:14 +0100

    added solution for problem 03. Variations without Repetition

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P03_Variations_without_Repetition/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace P03_Variations_without_Repetition
+{
+    using System;
+
+    internal class StartUp
+    {
+
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] used;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(); 
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            used = new bool[elements.Length];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[index] = elements[i];
+                    Variations(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
