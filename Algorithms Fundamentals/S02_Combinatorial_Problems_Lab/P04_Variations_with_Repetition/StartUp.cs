3f1f30fb52a43d794509c90cb4f051ea9335edb9
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-09 10:54:22 +0100

    added solution for problem 04. Variations with Repetition

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P04_Variations_with_Repetition/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace P04_Variations_with_Repetition
+{
+    using System;
+
+    internal class StartUp
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
