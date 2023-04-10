9d1d148af3df97b62034fc84ca1f81f5d7ab3a83
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-10 09:02:48 +0100

    added solution for problem 07. N Choose K Count

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P07_N_Choose_K_Count/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P07_N_Choose_K_Count
+{
+    using System;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            var top = GetFactorial(n);
+            var bottom = GetFactorial(k) * GetFactorial(n - k);
+
+            Console.WriteLine(top / bottom);
+        }
+
+        private static double GetFactorial(int number)
+        {
+            if (number == 1)
+                return 1;
+            else
+                return number * GetFactorial(number - 1);
+        }
+
+    }
+}
