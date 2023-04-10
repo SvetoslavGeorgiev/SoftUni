f6407ea08b4293b4afe8503927ba106dd00ae661
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-10 09:02:23 +0100

    added solution for problem 06. Combinations with Repetition

---
+++ Algorithms Fundamentals/S02_Combinatorial_Problems_Lab/P06_Combinations_with_Repetition/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace P06_Combinations_with_Repetition
+{
+    using System;
+    internal class StartUp
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            GetCombinations(0, 0);
+        }
+
+        private static void GetCombinations(int index, int startIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = startIndex; i < elements.Length; i++)
+            {
+
+                combinations[index] = elements[i];
+                GetCombinations(index + 1, i);
+            }
+        }
+    }
+}
