2bf59552762fef7c45ea8fc9b4254de02f5db407
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-10 15:33:11 +0100

    added solution for problem 02. String Length

---
+++ JS_Advanced/S01_Syntax_Functions_and_Statements_Lab/P02_String_Length/String_Length.js
@@ -0,0 +1,10 @@
+function solve(input1, input2, input3) {
+
+    let sum = input1.length + input2.length + input3.length;
+    let avg = Math.floor(sum / 3)
+
+    console.log(sum);
+    console.log(avg);
+}
+
+solve('chocolate', 'ice cream', 'cake');
