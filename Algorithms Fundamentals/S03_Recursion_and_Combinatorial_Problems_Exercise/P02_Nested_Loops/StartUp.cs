745db5b42c413cc47b32ab53446e51eca39503d5
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-11 23:51:54 +0100

    added solution for problem P02_Nested_Loops

---
+++ Algorithms Fundamentals/S03_Recursion_and_Combinatorial_Problems_Exercise/P02_Nested_Loops/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace P02_Nested_Loops
+{
+    using System;
+
+    internal class StartUp
+    {
+        private static int n;
+        private static int k;
+        private static int[] elements;
+        private static int[] variations;
+
+
+        static void Main(string[] args)
+        {
+            
+            
+            n = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                elements[i] = i + 1;
+            }
+
+            k = n;
+
+            variations = new int[k];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
