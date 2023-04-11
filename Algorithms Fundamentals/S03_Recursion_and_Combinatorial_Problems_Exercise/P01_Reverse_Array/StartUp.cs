a69282d3dbf71c57e115bd1ab37cbbc1332d5f11
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-11 23:27:59 +0100

    added solution for problem P01_Reverse_Array

---
+++ Algorithms Fundamentals/S03_Recursion_and_Combinatorial_Problems_Exercise/P01_Reverse_Array/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace P01_Reverse_Array
+{
+    using System;
+    using System.Linq;
+
+    internal class StartUp
+    {
+
+        public static string[] arr;
+
+        static void Main(string[] args)
+        {
+            arr = Console.ReadLine().Split();
+
+
+            ReverseArray(0);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void ReverseArray(int index)
+        {
+
+            if (index == arr.Length / 2)
+            {
+                return;
+            }
+
+            var temp = arr[index];
+            arr[index] = arr[arr.Length - 1 - index];
+            arr[arr.Length - 1 - index] = temp;
+
+            ReverseArray(index + 1);
+
+        }
+    }
+}
