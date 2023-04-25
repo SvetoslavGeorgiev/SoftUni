919535e90660ef961950df70292e1f03069d5615
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-25 08:43:38 +0100

    added solution for problem P07_Piece_of_Pie

---
+++ JS_Advanced/S03_Arrays_and_Nested_Arrays_Lab/P07_Piece_of_Pie/Piece_of_Pie.js
@@ -0,0 +1,24 @@
+function solve(arr, start, end){
+
+    return arr.slice(arr.indexOf(start), arr.indexOf(end) + 1)
+
+}
+
+console.log(solve(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+));
+
+console.log(solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie'
+));
