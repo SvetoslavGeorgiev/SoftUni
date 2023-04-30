2fd7c4ff988fd6e26dfe339cbcd2a3cfd11c80df
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-04-30 14:00:57 +0100

    added solution for problem P01_Print_an_Array_with_a_Given_Delimiter

---
+++ JS_Advanced/S04_Arrays_and_Nested_Arrays_Exercise/P01_Print_an_Array_with_a_Given_Delimiter/Print_an_Array_with_a_Given_Delimiter.js
@@ -0,0 +1,26 @@
+function solve(input, separator){
+
+    console.log(input.join(`${separator}`));
+
+}
+
+
+
+
+
+solve(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-')
+
+
+solve(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
