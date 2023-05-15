1a3d3bd2256229078c46e0c34a4a11ffad68d95b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-05-15 23:08:56 +0100

    added new solution for P03_Sum_Numbers

---
+++ JS_Advanced/S07_DOM_Introduction_Lab/P03_Sum_Numbers/Sum_Numbers.js
@@ -0,0 +1,7 @@
+function calc(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let sum = document.getElementById("sum");
+
+    sum.value = num1 + num2;
+}
