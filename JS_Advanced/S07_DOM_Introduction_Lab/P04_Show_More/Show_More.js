3fd0819f05bb72f265613d9834829295b756fac3
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-05-16 00:34:52 +0100

    added new solution for P04_Show_More

---
+++ JS_Advanced/S07_DOM_Introduction_Lab/P04_Show_More/Show_More.js
@@ -0,0 +1,8 @@
+function showText() {
+    let showText = document.getElementById("text");
+    showText.style.display = "inline"
+
+    let readMore = document.getElementById("more");
+    readMore.style.display = "none"
+    
+}
