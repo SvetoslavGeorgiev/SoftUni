5514774e11ae249cd084f56b9ea53b7b738e2272
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-05-15 23:08:33 +0100

    added new solution for P02_Collect_List

---
+++ JS_Advanced/S07_DOM_Introduction_Lab/P02_Collect_List/Collect_List.js
@@ -0,0 +1,6 @@
+function getText() {
+    let ulElement = document.getElementById('items');
+
+    let textareaElement = document.getElementById('result');
+    textareaElement.textContent = ulElement.textContent;
+}
