50ab15456836791373c0adbbb0c861eaaeacfcc6
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-08-07 04:04:50 +0100

    implementing translation for the web site home pages and nav bars

---
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem/Controllers/LangController.cs
@@ -0,0 +1,26 @@
+﻿namespace SchoolMealsOrderingSystem.Controllers
+{
+    using Microsoft.AspNetCore.Localization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class LangController : Controller
+    {
+        public async Task<IActionResult> ChangeLanguage(string culture)
+        {
+            Console.WriteLine("new selected language: " + culture);
+
+            if (!string.IsNullOrEmpty(culture))
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+            }
+
+            var returnUrl = Request.Headers["Referer"].ToString() ?? "/";
+
+            return Redirect(returnUrl);
+        }
+    }
+}
--- C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem/StartUp.cs
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem/StartUp.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using SchoolMealsOrderingSystem.Core.Contracts;
 using SchoolMealsOrderingSystem.Core.Services;
 using SchoolMealsOrderingSystem.Data;
 using SchoolMealsOrderingSystem.Data.Entities;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using static SchoolMealsOrderingSystem.Data.Constants.GeneralConstants;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -38,7 +44,10 @@ builder.Services.ConfigureApplicationCookie(options =>
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
-});
+})
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
 
 builder.Services.AddScoped<ISchoolServices, SchoolServices>();
 builder.Services.AddScoped<IChildServices, ChildServices>();
@@ -49,6 +58,13 @@ builder.Services.AddScoped<IDailyMenuServices, DailyManuServices>();
 
 
 var app = builder.Build();
+
+var supportedCultures = new[] { "en", "bg" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 
 if (app.Environment.IsDevelopment())
@@ -79,9 +95,6 @@ app.UseEndpoints(endpoints =>
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
-
-    
-
     endpoints.MapDefaultControllerRoute();
 });
 
