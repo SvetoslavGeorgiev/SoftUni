683954e32bfa8e4fa007f0391c95de5f3721d3ad
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-08-26 23:18:59 +0100

    added new child card design and swiper for the parent children view

---
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem/wwwroot/js/swiperScript.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".slide-content", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
