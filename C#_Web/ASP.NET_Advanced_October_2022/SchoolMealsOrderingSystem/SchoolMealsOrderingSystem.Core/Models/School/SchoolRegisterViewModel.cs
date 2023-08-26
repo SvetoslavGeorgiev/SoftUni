8a814b9329f426837a4690a8b4d740fab0ce6e69
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-08-26 13:15:20 +0100

    added localization for school sign up Data Annotations and school login Data Annotations

--- C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Core/Models/School/SchoolRegisterViewModel.cs
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Core/Models/School/SchoolRegisterViewModel.cs
@@ -6,21 +6,21 @@
     public class SchoolRegisterViewModel
     {
         [Required(ErrorMessage = SchoolNameRequired)]
-        [StringLength(SchoolNameMaxLength, MinimumLength = SchoolNameMinLength)]
+        [StringLength(SchoolNameMaxLength, MinimumLength = SchoolNameMinLength, ErrorMessage = FieldSymbolsLength)]
         public string SchoolName { get; set; } = null!;
 
         [Required(ErrorMessage = EmailRequired)]
         [EmailAddress(ErrorMessage = InvalidEmail)]
-        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength)]
         public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = PasswordRequired)]
-        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength, ErrorMessage = FieldSymbolsLength)]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = PasswordAndConfirmPasswordEquality)]
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = PasswordConfirmRequired)]
         public string ConfirmPassword { get; set; } = null!;
 
     }
