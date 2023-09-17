ec2a496aa94b77b2d868f02f9ccd082212248559
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2023-09-17 20:51:12 +0100

    added Globalization and Localization for EditSchoolProfile and for it's model DataAnnotations

--- C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Core/Models/School/EditSchoolUserViewModel.cs
+++ C#_Web/ASP.NET_Advanced_October_2022/SchoolMealsOrderingSystem/SchoolMealsOrderingSystem.Core/Models/School/EditSchoolUserViewModel.cs
@@ -23,8 +23,9 @@
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = PasswordAndConfirmPasswordEquality)]
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = PasswordConfirmRequired)]
         public string ConfirmPassword { get; set; } = null!;
 
 
