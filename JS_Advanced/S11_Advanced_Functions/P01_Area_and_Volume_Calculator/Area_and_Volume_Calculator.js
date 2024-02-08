b5a7bbcb2ec5a0da77df5958dbae76f103a0f19b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2024-02-08 12:24:18 +0000

    update

---
+++ JS_Advanced/S11_Advanced_Functions/P01_Area_and_Volume_Calculator/Area_and_Volume_Calculator.js
@@ -0,0 +1,25 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+function solve(area, vol, input) {
+    return JSON.stringify(JSON.parse(input)
+        .map(x=> ({
+            area: area.call(x),
+            volume: vol.call(x)
+        })));
+}
+
+console.log(solve(area, vol, `[
+    {"x":"10","y":"-22","z":"10"},
+    {"x":"47","y":"7","z":"-5"},
+    {"x":"55","y":"8","z":"0"},
+    {"x":"100","y":"100","z":"100"},
+    {"x":"55","y":"80","z":"250"}
+    ]`
+    ));
+
