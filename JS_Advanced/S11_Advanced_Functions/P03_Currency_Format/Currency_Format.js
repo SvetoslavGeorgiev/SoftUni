3f0eb7bb0d4845a4130863165dec372754cf4b3b
S.Georgiev <svetoslavgeorgiev86@gmail.com>
2024-03-23 19:35:51 +0000

    added solution for problem 3-CurrencyFormatter

---
+++ JS_Advanced/S11_Advanced_Functions/P03_Currency_Format/Currency_Format.js
@@ -0,0 +1,22 @@
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+
+function createFormatter(separator,
+    symbol,
+    symbolFirst,
+    formatter) {
+    return (value) => formatter(separator,
+         symbol,
+         symbolFirst,
+         value);
+}
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
